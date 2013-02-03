5c9003097d9008be245a7a15c9fde78ebb7f4184
Jan Griesel <jan.griesel@plentymarkets.com>
2013-02-03 22:41:31 +0100

    add more usefull comments

--- config/basic.inc.php
+++ config/basic.inc.php
@@ -1,7 +1,14 @@
 <?php
 
+/*
+ * standard time zones of plentymarkets
+ */
 date_default_timezone_set('Europe/Berlin');
 
+/*
+ * this link is used in all standard classes
+ * it points to the base dir of this project
+ */
 define('ROOT', realpath(dirname(__FILE__).'/../').'/');
 
 ?>
