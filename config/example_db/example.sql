87efba191b251374fc73e45a0948b6b7e6041d0e
Jan Griesel <jan.griesel@plentymarkets.com>
2013-02-03 22:34:30 +0100

    add comments with the class name, which use the data tables

--- config/example_db/example.sql
+++ config/example_db/example.sql
@@ -1,15 +1,23 @@
 
 CREATE DATABASE `soap_db`; /*!40100 DEFAULT CHARACTER SET utf8 */
 
-CREATE TABLE `plenty_soap_token` (
+/**
+ * used by StoreToken.class.php
+ */
+CREATE TABLE `soap_db`.`plenty_soap_token` (
   `soap_token_user` varchar(64) NOT NULL,
   `soap_token_inserted` datetime DEFAULT NULL,
   `soap_token` varchar(32) DEFAULT NULL,
   `soap_token_user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`soap_token_user`),
   KEY `inserted` (`soap_token_inserted`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB 
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
 
+/**
+ * used by SoapCall_GetCountriesOfDelivery.class.php
+ */
 CREATE  TABLE `soap_db`.`plenty_countries_of_delivery` (
   `country_id` INT NOT NULL ,
   `active` INT NULL ,
@@ -21,39 +29,56 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_unicode_ci;
 
+/**
+ * used by SoapCall_GetMethodOfPayments.class.php
+ */
 CREATE  TABLE `soap_db`.`plenty_method_of_payments` (
   `method_of_payment_id` INT NOT NULL ,
   `method_of_payment_name` VARCHAR(64) NULL ,
   `active_countries` VARCHAR(256) NULL ,
   `active_multishops` VARCHAR(120) NULL ,
   PRIMARY KEY (`method_of_payment_id`) ,
-  UNIQUE INDEX `method_of_payment_id_UNIQUE` (`method_of_payment_id` ASC) )
+  UNIQUE INDEX `method_of_payment_id_UNIQUE` (`method_of_payment_id` ASC) 
+) ENGINE=InnoDB 
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_unicode_ci;
 
+/**
+ * used by SoapCall_GetOrderStatusList.class.php
+ */
 CREATE TABLE `plenty_order_status` (
   `order_status` decimal(4,2) NOT NULL,
   `lang` char(2) COLLATE utf8_unicode_ci NOT NULL,
   `status_name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`order_status`,`lang`),
   KEY `order_status_lang_INDEX` (`order_status`,`lang`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB 
+DEFAULT CHARSET = utf8 
+COLLATE = utf8_unicode_ci;
 
+/**
+ * used by SoapCall_GetWarehouseList.class.php
+ */
 CREATE  TABLE `soap_db`.`plenty_warehouse` (
   `warehouse_id` INT NOT NULL ,
   `warehouse_type` INT NULL ,
   `warehouse_name` VARCHAR(40) NULL ,
   PRIMARY KEY (`warehouse_id`) ,
-  UNIQUE INDEX `warehouse_id_UNIQUE` (`warehouse_id` ASC) )
+  UNIQUE INDEX `warehouse_id_UNIQUE` (`warehouse_id` ASC) 
+) ENGINE=InnoDB 
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_unicode_ci;
 
+/**
+ * SoapCall_GetVATConfig.class.php
+ */
 CREATE  TABLE `soap_db`.`plenty_vat_config` (
   `country_id` INT NOT NULL ,
   `vat_id` INT NOT NULL ,
   `vat_value` DECIMAL(8,4) NULL ,
   PRIMARY KEY (`country_id`, `vat_id`) ,
-  UNIQUE INDEX `country_vat_UNIQUE` (`country_id` ASC, `vat_id` ASC) )
+  UNIQUE INDEX `country_vat_UNIQUE` (`country_id` ASC, `vat_id` ASC) 
+) ENGINE=InnoDB 
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_unicode_ci;
 
