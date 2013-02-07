0975b79e2b2564b73a5736bc13a40e4dc08985cb
Jan Griesel <jan.griesel@plentymarkets.com>
2013-02-07 23:44:30 +0100

    add function to finish daemon in an correct way

--- cli/PlentySoap.daemon.php
+++ cli/PlentySoap.daemon.php
@@ -8,6 +8,31 @@
 require_once realpath(dirname(__FILE__).'/../').'/config/basic.inc.php';
 require_once ROOT.'daemon/PlentySoapDaemon.class.php';
 
+
+/**
+ * This function is important in order to finish the daemon process in a correct way
+ */
+if (function_exists('pcntl_signal'))
+{
+	declare(ticks = 1);
+
+	function plentySoapDaemonSigHandler($signo)
+	{
+		switch ($signo)
+		{
+			case SIGTERM:
+			case SIGINT:
+				PlentySoapDaemon::getInstance()->stopDaemon();
+				break;
+		}
+	}
+	pcntl_signal(SIGTERM, 'plentySoapDaemonSigHandler');
+	pcntl_signal(SIGINT, 'plentySoapDaemonSigHandler');
+}
+
+/**
+ * Start daemon
+ */
 PlentySoapDaemon::getInstance()->run();
 
 ?>
