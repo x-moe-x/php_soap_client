7dda39c35ef1e3fe4cb83e9b1b0c6b3989c9bdbd
x-moe-x <moritz@net-xpress.de>
2013-04-09 16:44:23 +0200

    added support to request multiple AttributeValuesSets at once

--- experiments/GetAttributeValueSets/Request_GetAttributeValueSets.class.php
+++ experiments/GetAttributeValueSets/Request_GetAttributeValueSets.class.php
@@ -4,19 +4,42 @@
 class Request_GetAttributeValueSets 
 {
 
-	public function getRequest($oAttributeValueSetID, $oLang = null)
+	public function getRequest($oAttributeValueSetIDs, $oLang = null)
 	{
-		$oPlentySoapRequest_GetAttributeValueSets = new PlentySoapRequest_GetAttributeValueSets();
 		
+		$oPlentySoapRequest_GetAttributeValueSets = new PlentySoapRequest_GetAttributeValueSets();
 		$oPlentySoapRequest_GetAttributeValueSets->AttributeValueSets = new ArrayOfPlentysoaprequestobject_getattributevaluesets();
 		
-		$oPlentySoapRequest_GetAttributeValueSets->AttributeValueSets->item = new PlentySoapRequestObject_GetAttributeValueSets();
-		
-		$oPlentySoapRequest_GetAttributeValueSets->AttributeValueSets->item->AttributeValueSetID = $oAttributeValueSetID;
-		 
-		$oPlentySoapRequest_GetAttributeValueSets->AttributeValueSets->item->Lang = $oLang;
+		if (isset($oAttributeValueSetIDs) && is_array($oAttributeValueSetIDs))
+		{
+			$oPlentySoapRequest_GetAttributeValueSets->AttributeValueSets->item = array();
 			
+			foreach ($oAttributeValueSetIDs as $currentID)
+			{
+				$currentItem = new PlentySoapRequestObject_GetAttributeValueSets();
+				$currentItem->AttributeValueSetID	= $currentID;
+				$currentItem->Lang 					= $oLang;
+				array_push($oPlentySoapRequest_GetAttributeValueSets->AttributeValueSets->item, $currentItem);				
+			} 
+		}
+		else if (isset($oAttributeValueSetIDs))
+		{
+			$oPlentySoapRequest_GetAttributeValueSets->AttributeValueSets->item = new PlentySoapRequestObject_GetAttributeValueSets();
+			
+			$oPlentySoapRequest_GetAttributeValueSets->AttributeValueSets->item->AttributeValueSetID = $oAttributeValueSetIDs;
+			 
+			$oPlentySoapRequest_GetAttributeValueSets->AttributeValueSets->item->Lang = $oLang;
+		}
+		else
+		{
+			// TODO error			
+		}
+		
 		return $oPlentySoapRequest_GetAttributeValueSets;
+		
+		
+		
+			
 	}
 	
 }
