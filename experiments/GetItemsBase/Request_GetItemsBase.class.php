0cbeaadfba422a8b62e9d6ea5827067796284d85
x-moe-x <moritz@net-xpress.de>
2013-04-16 14:53:50 +0200

    added full qualified GetItemsBase request

--- experiments/GetItemsBase/Request_GetItemsBase.class.php
+++ experiments/GetItemsBase/Request_GetItemsBase.class.php
@@ -6,9 +6,97 @@ class Request_GetItemsBase
 
 	public function getRequest()
 	{
-		$oPlentySoapRequest_GetItemsBase = new PlentySoapRequest_GetItemsBase();;
-		
-		return $oPlentySoapRequest_GetItemsBase;			
+		$oPlentySoapRequest_GetItemsBase = new PlentySoapRequest_GetItemsBase();
+
+		$oPlentySoapRequest_GetItemsBase->Page = 0;
+
+		$oPlentySoapRequest_GetItemsBase->ItemID = null;
+
+		$oPlentySoapRequest_GetItemsBase->LastUpdateFrom = null;
+
+		$oPlentySoapRequest_GetItemsBase->LastUpdateTill = null;
+
+		$oPlentySoapRequest_GetItemsBase->Marking1ID = null;
+
+		$oPlentySoapRequest_GetItemsBase->Marking2ID = null;
+
+		$oPlentySoapRequest_GetItemsBase->CategoriePath = null;
+
+		$oPlentySoapRequest_GetItemsBase->EAN1 = null;
+
+		$oPlentySoapRequest_GetItemsBase->ExternalItemID = null;
+
+		$oPlentySoapRequest_GetItemsBase->ItemNo = null;
+
+		$oPlentySoapRequest_GetItemsBase->Lang = null;
+
+		$oPlentySoapRequest_GetItemsBase->GetAttributeValueSets = false;
+
+		$oPlentySoapRequest_GetItemsBase->GetCategories = false;
+
+		$oPlentySoapRequest_GetItemsBase->GetItemAttributeMarkup = false;
+
+		$oPlentySoapRequest_GetItemsBase->GetItemOthers = false;
+
+		$oPlentySoapRequest_GetItemsBase->GetItemProperties = false;
+
+		$oPlentySoapRequest_GetItemsBase->GetItemSuppliers = false;
+
+		$oPlentySoapRequest_GetItemsBase->GetLongDescription = false;
+
+		$oPlentySoapRequest_GetItemsBase->GetMetaDescription = false;
+
+		$oPlentySoapRequest_GetItemsBase->GetShortDescription = false;
+
+		$oPlentySoapRequest_GetItemsBase->GetTechnicalData = false;
+
+		$oPlentySoapRequest_GetItemsBase->GetItemURL = null;
+
+		$oPlentySoapRequest_GetItemsBase->Gimahhot = null;
+
+		$oPlentySoapRequest_GetItemsBase->GoogleProducts = null;
+
+		$oPlentySoapRequest_GetItemsBase->Hitmeister = null;
+
+		$oPlentySoapRequest_GetItemsBase->Inactive = null;
+
+		$oPlentySoapRequest_GetItemsBase->Laary = null;
+
+		$oPlentySoapRequest_GetItemsBase->LastInsertedFrom = null;
+
+		$oPlentySoapRequest_GetItemsBase->LastInsertedTill = null;
+
+		$oPlentySoapRequest_GetItemsBase->MainWarehouseID = null;
+
+		$oPlentySoapRequest_GetItemsBase->Moebelprofi = null;
+
+		$oPlentySoapRequest_GetItemsBase->ProducerID = null;
+
+		$oPlentySoapRequest_GetItemsBase->Referrer = null;
+
+		$oPlentySoapRequest_GetItemsBase->Restposten = null;
+
+		$oPlentySoapRequest_GetItemsBase->ShopShare = null;
+
+		$oPlentySoapRequest_GetItemsBase->Shopgate = null;
+
+		$oPlentySoapRequest_GetItemsBase->Shopperella = null;
+
+		$oPlentySoapRequest_GetItemsBase->StockAvailable = null;
+
+		$oPlentySoapRequest_GetItemsBase->SumoScout = null;
+
+		$oPlentySoapRequest_GetItemsBase->Tradoria = null;
+
+		$oPlentySoapRequest_GetItemsBase->WebAPI = null;
+
+		$oPlentySoapRequest_GetItemsBase->Webshop = null;
+
+		$oPlentySoapRequest_GetItemsBase->Yatego = null;
+
+		$oPlentySoapRequest_GetItemsBase->Zalando = null;
+
+		return $oPlentySoapRequest_GetItemsBase;
 	}
 	
 }
