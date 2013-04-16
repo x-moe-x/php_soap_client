4adb1fc3a5b8f2b434a5ad50976aa122261de6a7
x-moe-x <moritz@net-xpress.de>
2013-04-16 15:35:29 +0200

    perform basic response interpretation added walking through pages

--- experiments/GetItemsBase/SoapCall_GetItemsBase.class.php
+++ experiments/GetItemsBase/SoapCall_GetItemsBase.class.php
@@ -7,6 +7,9 @@ require_once 'Request_GetItemsBase.class.php';
 
 class SoapCall_GetItemsBase extends PlentySoapCall
 {
+	private $page								=	0;
+	private $pages								=	-1;
+	private $oPlentySoapRequest_GetItemsBase	=	null;
 
 	public function __construct()
 	{
@@ -17,28 +20,111 @@ class SoapCall_GetItemsBase extends PlentySoapCall
 	{
 		$this->getLogger()->debug(__FUNCTION__);
 
-		try
+		if ($this->pages == -1)
 		{
-			/*
-			 * do soap call
-			*/
-
-			$oPlentySoapRequest_GetItemsBase = new Request_GetItemsBase();
-
-			$response		=	$this->getPlentySoap()->GetItemsBase($oPlentySoapRequest_GetItemsBase->getRequest());
-
-			if( $response->Success == true )
+			try
 			{
-				$this->getLogger()->debug(__FUNCTION__.' Request Success');
+
+				$oRequest_GetItemsBase = new Request_GetItemsBase();
+
+				$this->oPlentySoapRequest_GetItemsBase = $oRequest_GetItemsBase->getRequest();
+
+				/*
+				 * do soap call
+				*/
+				$response		=	$this->getPlentySoap()->GetItemsBase($this->oPlentySoapRequest_GetItemsBase);
+
+				if( $response->Success == true )
+				{
+					$articlesFound		= 	count($response->ItemsBase->item);
+					$pagesFound			=	$response->Pages;
+
+					$this->getLogger()->debug(__FUNCTION__.' Request Success - articles found : '.$articlesFound .' / pages : '.$pagesFound );
+
+					// process response
+					$this->responseInterpretation($response);
+
+					if ( $pagesFound > $this->page )
+					{
+						$this->page		=	1;
+						$this->pages	=	$pagesFound;
+
+						$this->executePages();
+
+					}
+				}
+				else
+				{
+					$this->getLogger()->debug(__FUNCTION__.' Request Error');
+				}
 			}
-			else
+			catch(Exception $e)
 			{
-				$this->getLogger()->debug(__FUNCTION__.' Request Error');
+				$this->onExceptionAction($e);
 			}
 		}
-		catch(Exception $e)
+		else
 		{
-			$this->onExceptionAction($e);
+			$this->executePages();
+		}
+	}
+
+	private function responseInterpretation($oPlentySoapResponse_GetItemsBase)
+	{
+		if( is_array( $oPlentySoapResponse_GetItemsBase->ItemsBase->item ) )
+		{
+			foreach( $oPlentySoapResponse_GetItemsBase->ItemsBase->item AS $itemsBase)
+			{
+				$this->processItemsBase($itemsBase);
+			}
+		}
+		else
+		{
+			$this->processItemsBase($oPlentySoapResponse_GetItemsBase->Orders->item);
+		}
+		$this->getLogger()->debug(__FUNCTION__.' : done' );
+	}
+
+	private function processItemsBase($oItemsBase)
+	{
+		$this->getLogger()->debug(__FUNCTION__.' : '
+				. 	' ItemID : '			.$oItemsBase->ItemID		.','
+				. 	' ItemNo : '			.$oItemsBase->ItemNo		.','
+				. 	' Name : '				.$oItemsBase->Texts->Name
+		);
+	}
+
+	private function executePages()
+	{
+		while ( $this->pages > $this->page )
+		{
+			$this->oPlentySoapRequest_GetItemsBase->Page = $this->page;
+			try
+			{
+				$response		=	$this->getPlentySoap()->GetItemsBase( $this->oPlentySoapRequest_GetItemsBase );
+
+				if( $response->Success == true )
+				{
+					$articlesFound	=	count($response->ItemsBase->item);
+					$this->getLogger()->debug(__FUNCTION__.' Request Success - articles found : '.$articlesFound .' / page : '.$this->page );
+
+					// auswerten
+					$this->responseInterpretation( $response);
+				}
+
+				$this->page++;
+
+			}
+			catch(Exception $e)
+			{
+				$this->onExceptionAction($e);
+			}
+
+			// TODO remove after debugging:
+			// stop after 3 pages
+			if ($this->page >= 3)
+				break;
+
 		}
 	}
 }
