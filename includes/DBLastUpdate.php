0e74874b9e57922cc05b03b2ddb2f5c712aebdef
x-moe-x <moritz@net-xpress.de>
2013-04-23 10:07:05 +0200

    added DBLastUpdate.php, contributing to issue #2

---
+++ includes/DBLastUpdate.php
@@ -0,0 +1,42 @@
+<?php
+function lastUpdateStart($functionName){
+	// get lastupdate
+	$query = 'SELECT * FROM MetaLastUpdate WHERE `Function` = \''. $functionName.'\'';
+
+	$result = DBQuery::getInstance()->select($query)->fetchAssoc();
+
+	$lastUpdate = intval($result['LastUpdate']);
+	$currentTime = time();
+
+	// store current timestamp
+	$query = 'REPLACE INTO `MetaLastUpdate` '.
+			DBUtils::buildInsert(
+					array(
+							'id'	=>  $result['id'],
+							'Function'	=>	$functionName,
+							'LastUpdate'	=> $lastUpdate,
+							'CurrentLastUpdate'	=> $currentTime
+					)
+			);
+
+	DBQuery::getInstance()->replace($query);
+
+	return array($lastUpdate, $currentTime, $result['id']);
+}
+
+function lastUpdateFinish($id, $currentTime, $functionName)
+{
+	// store current timestamp
+	$query = 'REPLACE INTO `MetaLastUpdate` '.
+			DBUtils::buildInsert(
+					array(
+							'id'	=>  $id,
+							'Function'	=>	$functionName,
+							'LastUpdate'	=> $currentTime,
+							'CurrentLastUpdate'	=> $currentTime
+					)
+			);
+
+	DBQuery::getInstance()->replace($query);
+}
+?>
