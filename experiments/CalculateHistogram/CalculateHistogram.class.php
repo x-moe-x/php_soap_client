61741811743a497904045c9232a337da3dbd87c8
x-moe-x <moritz@net-xpress.de>
2013-04-23 15:42:31 +0200

    added empty CalculateHistogram class, contribute to #5

---
+++ experiments/CalculateHistogram/CalculateHistogram.class.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * 1. retrteive data
+ *
+ * 2. stuff in histogram
+ *
+ * 3. show results
+ *
+ * @author x-moe-x
+ * @copyright net-xpress GmbH & Co. KG www.net-xpress.com
+ */
+class CalculateHistogram
+{
+	/**
+	 *
+	 * @var string
+	 */
+	private $identifier4Logger = '';
+
+	public function __construct()
+	{
+		$this->identifier4Logger = __CLASS__;
+	}
+
+	public function execute()
+	{
+		$this->getLogger()->debug(__FUNCTION__.' : CalculateHistogram' );
+	}
+
+	/**
+	 *
+	 * @return Logger
+	 */
+	protected function getLogger()
+	{
+		return Logger::instance($this->identifier4Logger);
+	}
+}
+
+
+?>
