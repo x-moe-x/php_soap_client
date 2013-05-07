74143965b02fb1bd032fdb958a56d3da22ef192f
x-moe-x <moritz@net-xpress.de>
2013-05-07 15:40:05 +0200

    streamlined query.sql

--- config/example_db/query.sql
+++ config/example_db/query.sql
@@ -1,17 +1,15 @@
 SELECT
+	OrderItem.ItemID,
 	SUM(CAST(OrderItem.Quantity AS SIGNED)) AS `quantity`,
-	CAST(GROUP_CONCAT(CAST(OrderItem.Quantity AS SIGNED) ORDER BY OrderItem.Quantity DESC SEPARATOR ', ') AS CHAR(200)) AS `quantities`,
+	COUNT(OrderHead.OrderID) AS `orders`,
+	(AVG(`quantity`) + STDDEV(`quantity`))*1.35 AS `range`,
+	CAST(GROUP_CONCAT(IF(OrderItem.Quantity > 0 ,CAST(OrderItem.Quantity AS SIGNED),NULL) ORDER BY OrderItem.Quantity DESC SEPARATOR ', ') AS CHAR) AS `quantities`,
 	FROM_UNIXTIME(OrderHead.OrderTimestamp, '%Y') AS `year`,
 	FROM_UNIXTIME(OrderHead.OrderTimestamp, '%m') AS `month`,
 	FROM_UNIXTIME(OrderHead.OrderTimestamp, '%d') AS `day`,
-	COUNT(OrderHead.OrderID) AS `orders`,
-	AVG(`quantity`) as `µ`,
-	STDDEV(`quantity`) AS `σ`,
-	(AVG(`quantity`) + STDDEV(`quantity`))*1.35 AS `range`,
-	OrderItem.ItemID,
 	ItemsBase.Marking1ID FROM OrderItem LEFT JOIN (OrderHead, ItemsBase) ON (OrderHead.OrderID = OrderItem.OrderID AND OrderItem.ItemID = ItemsBase.ItemID)
 WHERE
-	(OrderHead.OrderTimestamp BETWEEN 1364767200 AND 1367334415) AND
+	(OrderHead.OrderTimestamp BETWEEN 1359669600 AND 1367334415) AND
 	(OrderHead.OrderStatus < 8 OR OrderHead.OrderStatus >= 9) AND
 	OrderType = 'order' AND
 	ItemsBase.Marking1ID IN (9,12,16,20) /* yellow, red, green, black */
