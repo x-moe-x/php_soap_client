c0de2d622737997122c127e6870bb55d97dea47d
x-moe-x <moritz@net-xpress.de>
2013-05-21 16:50:12 +0200

    striped unnecessary information from query

--- config/example_db/query.sql
+++ config/example_db/query.sql
@@ -1,18 +1,15 @@
 SELECT
 	OrderItem.ItemID,
 	SUM(CAST(OrderItem.Quantity AS SIGNED)) AS `quantity`,
-	COUNT(OrderHead.OrderID) AS `orders`,
 	(AVG(`quantity`) + STDDEV(`quantity`))*1.35 AS `range`,
-	DATEDIFF(FROM_UNIXTIME(1367334415),FROM_UNIXTIME(OrderHead.OrderTimestamp)) DIV 30.5 AS `intervall`,
-	CAST(GROUP_CONCAT(IF(OrderItem.Quantity > 0 ,CAST(OrderItem.Quantity AS SIGNED),NULL) ORDER BY OrderItem.Quantity DESC SEPARATOR ', ') AS CHAR) AS `quantities`,
+	CAST(GROUP_CONCAT(IF(OrderItem.Quantity > 0 ,CAST(OrderItem.Quantity AS SIGNED),NULL) ORDER BY OrderItem.Quantity DESC SEPARATOR ",") AS CHAR) AS `quantities`,
 	ItemsBase.Marking1ID FROM OrderItem LEFT JOIN (OrderHead, ItemsBase) ON (OrderHead.OrderID = OrderItem.OrderID AND OrderItem.ItemID = ItemsBase.ItemID)
 WHERE
-	(OrderHead.OrderTimestamp BETWEEN 1359669600 AND 1367334415) AND
+	(OrderHead.OrderTimestamp BETWEEN 1367334415-(60*60*24*90) AND 1367334415) AND
 	(OrderHead.OrderStatus < 8 OR OrderHead.OrderStatus >= 9) AND
-	OrderType = 'order' AND
+	OrderType = "order" AND
 	ItemsBase.Marking1ID IN (9,12,16,20) /* yellow, red, green, black */
 GROUP BY
-	OrderItem.ItemID,
-	`intervall`
+	OrderItem.ItemID
 ORDER BY
-	ItemID, `intervall` LIMIT 1000
+	ItemID LIMIT 1000
