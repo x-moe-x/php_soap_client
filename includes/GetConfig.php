111bb786f40f3753f93063781190643bf9ab1d0b
Moritz Gericke <xmoexdev@gmail.com>
2014-01-07 14:13:39 +0100

    work in progress DELETE THIS COMMIT AFTER CLEANUP

--- includes/GetConfig.php
+++ includes/GetConfig.php
@@ -6,111 +6,132 @@ class Config {
 	/**
 	 * sets the value of a given config-key
 	 */
-	function set($key, $value) {
-		$query = 'SELECT `ConfigType`, `Active`, `ConfigValue` FROM `MetaConfig` WHERE `ConfigKey` = ' . $key . ' LIMIT 0,2';
+	public static function set($key, $value) {
+		$query = 'SELECT `ConfigType`, `Active`, `ConfigValue` FROM `MetaConfig` WHERE `ConfigKey` = \'' . $key . '\' LIMIT 0,2';
 		$queryResult = DBQuery::getInstance() -> select($query);
 
 		// if key in db ...
-		if ($queryResult -> getNumRows() !== 1) {
+		if ($queryResult -> getNumRows() === 1) {
+			
 			$currentConfig = $queryResult -> fetchAssoc();
+			$oldValue = $currentConfig['ConfigType'] === 'int' ? intval($currentConfig['ConfigValue']) : floatval($currentConfig['ConfigValue']);
 
 			// ... then check if key is active  ...
-			if ($currentConfig['Active'] === 1) {
+			if (intval($currentConfig['Active']) === 1) {
+
+				$parsedValue = $currentConfig['ConfigType'] === 'int' ? intval($value) : floatval($value);
 
 				// ... then check if type matches ...
-				if ((($currentConfig['ConfigType'] === 'int') && (is_float($value))) xor (($currentConfig['ConfigType'] === 'float') && (is_int($value)))) {
+				if ((($currentConfig['ConfigType'] === 'int') && (is_int($parsedValue))) xor (($currentConfig['ConfigType'] === 'float') && (is_float($parsedValue)))) {
 
 					// ... then set given value, update timestamp and make log entry
-					$oldValue = $currentConfig['ConfigValue'];
-					$updateQuery = 'UPDATE `MetaConfig` SET `ConfigValue` = ' . $value . ', `LastUpdate` = ' . time() . ' WHERE `ConfigKey` = ' . $key;
+					$updateQuery = 'UPDATE `MetaConfig` SET `ConfigValue` = ' . $value . ', `LastUpdate` = ' . time() . ' WHERE `ConfigKey` = \'' . $key . '\'';
 					DBQuery::getInstance() -> update($updateQuery);
-					$this -> getLogger() -> debug(__FUNCTION__ . ' Changed value of key ' . $key . ' from ' . $oldValue . ' to ' . $value);
-					if ($currentConfig['ConfigType'] === 'int') {
-						return intval($value);
-					} else {
-						return floatval($value);
-					}
+					Config::getLogger() -> debug(__FUNCTION__ . ' Changed value of key ' . $key . ' from ' . $oldValue . ' to ' . $value);
+					return $parsedValue;
 				} else {
 					// ... otherwise trow exception and make log entry
-					$this -> getLogger() -> err(__FUNCTION__ . ' Type of ' . $value . ' does not match ' . $currentConfig['ConfigType']);
+					Config::getLogger() -> debug(__FUNCTION__ . ' Type of ' . $value . ' does not match ' . $currentConfig['ConfigType']);
 					throw new Exception('Type of ' . $value . ' does not match ' . $currentConfig['ConfigType']);
+					return $oldValue;
 				}
 			} else {
 				// ... otherwise don't change value and make log entry
-				$this -> getLogger() -> debug(__FUNCTION__ . ' Key ' . $key . ' is inactive and value ' . $value . ' is not applied');
-				if ($currentConfig['ConfigType'] === 'int') {
-					return intval($currentConfig['ConfigValue']);
-				} else {
-					return floatval($currentConfig['ConfigValue']);
-				}
+				Config::getLogger() -> debug(__FUNCTION__ . ' Key ' . $key . ' is inactive and value ' . $value . ' is not applied');
+				return $oldValue;
 			}
 		} else {
 			// ... otherwise throw exception and make log entry
-			$this -> getLogger() -> err(__FUNCTION__ . ' Could not resolve desired key ' . $key);
+			Config::getLogger() -> debug(__FUNCTION__ . ' Could not resolve desired key ' . $key);
 			throw new Exception('Could not resolve desired key ' . $key);
+			return null;
 		}
 	}
 
 	/**
-	 * gets the value of a given config key
+	 * get all config values at once
 	 */
-	function get($key) {
-		// if key in db ...
-		// ... then check if key is active ...
-		// ... ... then return value
-		// ... ... otherwise return 'inactive'-constant
-		// ... otherwise throw exception
+	public static function getAll() {
+		$query = 'SELECT * FROM `MetaConfig`';
+		$queryResult = DBQuery::getInstance() -> select($query);
+
+		$result = array();
+		while ($currentConfig = $queryResult -> fetchAssoc()) {
+			$parsedValue = $currentConfig['ConfigType'] === 'int' ? intval($currentConfig['ConfigValue']) : floatval($currentConfig['ConfigValue']);
+			$result[$currentConfig['ConfigKey']]['Value'] = $parsedValue;
+			$result[$currentConfig['ConfigKey']]['Active'] = intval($currentConfig['Active']);
+		}
+		return $result;
 	}
 
-	protected function getLogger() {
+	/**
+	 * get the value of a specific config key
+	 */
+	public static function get($key) {
+		$config = Config::getAll();
+
+		// if key in db ...
+		if (in_array($key, $config)) {
+			// ... then check if key is active ...
+			if ($config[$key]['Active'] === 1) {
+				// ... ... then return value
+				return $config[$key]['Value'];
+			} else {
+				// ... ... otherwise return 'inactive'-constant
+				return 'inactive';
+			}
+		} else {
+			// ... otherwise throw exception
+			Config::getLogger() -> debug(__FUNCTION__ . ' Could not resolve desired key ' . $key);
+			throw new Exception('Could not resolve desired key ' . $key);
+		}
+	}
+
+	protected static function getLogger() {
 		return Logger::instance(__CLASS__);
 	}
 
 }
 
-/**
- * extract config values form database
- *
- * @return array of config keys mapped on config values
- */
-function getConfig() {
-	$intValues = array('CalculationTimeA', 'CalculationTimeB', 'MinimumToleratedSpikesA', 'MinimumToleratedSpikesB', 'MinimumOrdersA', 'MinimumOrdersB');
-	$floatValues = array('SpikeTolerance', 'StandardDeviationFactor');
+/*
 
-	$query = 'SELECT
-				* FROM `MetaConfig`
-				WHERE';
+ function getConfig() {
+ $intValues = array('CalculationTimeA', 'CalculationTimeB', 'MinimumToleratedSpikesA', 'MinimumToleratedSpikesB', 'MinimumOrdersA', 'MinimumOrdersB');
+ $floatValues = array('SpikeTolerance', 'StandardDeviationFactor');
 
-	$nrOfIntVals = count($intValues);
-	$nrOfFloatVals = count($floatValues);
-	for ($i = 0; $i < $nrOfIntVals; ++$i){
-		$query .= '
-					`ConfigKey` = "'. $intValues[$i] .'" OR';
-	}
-	for ($i = 0; $i < $nrOfFloatVals; ++$i){
-		$query .= '
-					`ConfigKey` = "'. $floatValues[$i] .'" ' . ($i + 1 < $nrOfFloatVals ? 'OR':'');
-	}
+ $query = 'SELECT
+ * FROM `MetaConfig`
+ WHERE';
 
-	$resultConfigQuery = DBQuery::getInstance() -> select($query);
+ $nrOfIntVals = count($intValues);
+ $nrOfFloatVals = count($floatValues);
+ for ($i = 0; $i < $nrOfIntVals; ++$i) {
+ $query .= '
+ `ConfigKey` = "' . $intValues[$i] . '" OR';
+ }
+ for ($i = 0; $i < $nrOfFloatVals; ++$i) {
+ $query .= '
+ `ConfigKey` = "' . $floatValues[$i] . '" ' . ($i + 1 < $nrOfFloatVals ? 'OR' : '');
+ }
 
-	$result = array();
-	for ($i = 0; $i < $resultConfigQuery -> getNumRows(); ++$i) {
-		$configRow = $resultConfigQuery -> fetchAssoc();
-		if ($configRow['ConfigKey'] == 'SpikeTolerance' || $configRow['ConfigKey'] == 'StandardDeviationFactor')
-			$result[$configRow['ConfigKey']]['Value'] = floatval($configRow['ConfigValue']);
-		else
-			$result[$configRow['ConfigKey']]['Value'] = intval($configRow['ConfigValue']);
+ $resultConfigQuery = DBQuery::getInstance() -> select($query);
 
-		$result[$configRow['ConfigKey']]['Active'] = intval($configRow['Active']);
-	}
+ $result = array();
+ for ($i = 0; $i < $resultConfigQuery -> getNumRows(); ++$i) {
+ $configRow = $resultConfigQuery -> fetchAssoc();
+ if ($configRow['ConfigKey'] == 'SpikeTolerance' || $configRow['ConfigKey'] == 'StandardDeviationFactor')
+ $result[$configRow['ConfigKey']]['Value'] = floatval($configRow['ConfigValue']);
+ else
+ $result[$configRow['ConfigKey']]['Value'] = intval($configRow['ConfigValue']);
 
-	foreach (array_merge($intValues, $floatValues) as $key) {
-		if (!isset($result[$key]))
-			throw new RuntimeException('Missing Config Key: ' . $key);
-	}
+ $result[$configRow['ConfigKey']]['Active'] = intval($configRow['Active']);
+ }
 
-	return $result;
-}
+ foreach (array_merge($intValues, $floatValues) as $key) {
+ if (!isset($result[$key]))
+ throw new RuntimeException('Missing Config Key: ' . $key);
+ }
 
+ return $result;
+ }*/
 ?>
--- webinterface/updateConfig.php5
+++ webinterface/updateConfig.php5
@@ -13,32 +13,43 @@ $ucfirstKey = ucfirst($key);
 $preJSON = array('Message' => null, 'Value' => null);
 
 if (isset($_POST['key']) && isset($_POST['value'])) {
-	if (in_array($key, $integerKeys)) {
-		$parsedValue = intval($_POST['value']);
-	} else if (in_array($_POST['key'], $floatKeys)) {
-		$parsedValue = floatval($_POST['value']);
-		if ($key === 'spikeTolerance')
-			$parsedValue /= 100.0;
-	} else {
-		// wrong value
-		$preJSON['Message'] = 'wrong key: ' . $key;
+	try {
+		if ($key !== 'spikeTolerance') {
+			$setResult = Config::set($ucfirstKey, $_POST['value']);
+		} else {
+			$setResult = Config::set($ucfirstKey, floatval($_POST['value']) / 100);
+		}
+	} catch (Exception $e) {
+		$preJSON['Message'] = $e -> getMessage();
+		$preJSON['Value'] = $setResult;
 	}
+	/*
+	 if (in_array($key, $integerKeys)) {
+	 $parsedValue = intval($_POST['value']);
+	 } else if (in_array($_POST['key'], $floatKeys)) {
+	 $parsedValue = floatval($_POST['value']);
+	 if ($key === 'spikeTolerance')
+	 $parsedValue /= 100.0;
+	 } else {
+	 // wrong value
+	 $preJSON['Message'] = 'wrong key: ' . $key;
+	 }
 
-	if (!isset($config[$ucfirstKey])) {
-		$preJSON['Message'] = 'wrong key: ' . $ucfirstKey;
-	} else if ($config[$ucfirstKey]['Active'] !== 1) {
-		$preJSON['Message'] = 'tried to modify inactive key: ' . $ucfirstKey;
-	} else if ((($ucfirstKey === 'CalculationTimeA') && ($parsedValue < $config['CalculationTimeB']['Value'])) || (($ucfirstKey === 'CalculationTimeB') && ($config['CalculationTimeA']['Value'] < $parsedValue))) {
-		if ($ucfirstKey === 'CalculationTimeA')
-			$preJSON['Value'] = $config['CalculationTimeA']['Value'];
-		else 
-			$preJSON['Value'] = $config['CalculationTimeB']['Value'];
-		$preJSON['Message'] = 'calculation time a < calculation time b';
-	} else {
-		$query = 'REPLACE INTO `MetaConfig` ' . DBUtils::buildInsert(array('ConfigKey' => $ucfirstKey, 'ConfigValue' => $parsedValue, 'Active' => 1));
-		$preJSON['Value'] = $parsedValue;
-		DBQuery::getInstance() -> replace($query);
-	}
+	 if (!isset($config[$ucfirstKey])) {
+	 $preJSON['Message'] = 'wrong key: ' . $ucfirstKey;
+	 } else if ($config[$ucfirstKey]['Active'] !== 1) {
+	 $preJSON['Message'] = 'tried to modify inactive key: ' . $ucfirstKey;
+	 } else if ((($ucfirstKey === 'CalculationTimeA') && ($parsedValue < $config['CalculationTimeB']['Value'])) || (($ucfirstKey === 'CalculationTimeB') && ($config['CalculationTimeA']['Value'] < $parsedValue))) {
+	 if ($ucfirstKey === 'CalculationTimeA')
+	 $preJSON['Value'] = $config['CalculationTimeA']['Value'];
+	 else
+	 $preJSON['Value'] = $config['CalculationTimeB']['Value'];
+	 $preJSON['Message'] = 'calculation time a < calculation time b';
+	 } else {
+	 $query = 'REPLACE INTO `MetaConfig` ' . DBUtils::buildInsert(array('ConfigKey' => $ucfirstKey, 'ConfigValue' => $parsedValue, 'Active' => 1));
+	 $preJSON['Value'] = $parsedValue;
+	 DBQuery::getInstance() -> replace($query);
+	 }*/
 } else {
 	$preJSON['Message'] = 'unsufficient arguments';
 }
