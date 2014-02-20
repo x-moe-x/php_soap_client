3a4f25a24a4dc9a607fe185896d4e61760408e8f
x-moe-x <xmoexdev@gmail.com>
2014-02-20 15:31:02 +0100

    streamlined bulk insertion preparation of data added actual bulk insertion

--- experiments/GetItemsSuppliers/SoapCall_GetItemsSuppliers.class.php
+++ experiments/GetItemsSuppliers/SoapCall_GetItemsSuppliers.class.php
@@ -27,97 +27,89 @@ class SoapCall_GetItemsSuppliers extends PlentySoapCall {
 			// for every 50 ItemIDs ...
 			for ($page = 0; $page < ceil($result -> getNumRows() / self::$MAX_PAGES); $page++) {
 
-				// ... perpare a separate request
+				// ... perpare a separate request ...
 				$oRequest_GetItemsSuppliers = new Request_GetItemsSuppliers();
 				while (!$oRequest_GetItemsSuppliers -> isFull() && $current = $result -> fetchAssoc()) {
 					$oRequest_GetItemsSuppliers -> addItemID($current['ItemID']);
 				}
 
-				/*
-				 * do soap call
-				 */
+				// ... then do soap call ...
 				$response = $this -> getPlentySoap() -> GetItemsSuppliers($oRequest_GetItemsSuppliers -> getRequest());
 
+				// if successful ...
 				if ($response -> Success == true) {
 					$this -> getLogger() -> debug(__FUNCTION__ . ' Request Success');
 
-					// process response
+					// ... then process response
 					$this -> responseInterpretation($response);
 				} else {
+
+					// ... otherwise log error and try next request
 					$this -> getLogger() -> debug(__FUNCTION__ . ' Request Error');
 				}
 			}
-			
-			echo implode(',', $this->aStoreData);
+
+			// when done store all retrieved data to db
+			$this -> storeToDB();
+
 		} catch(Exception $e) {
 			$this -> onExceptionAction($e);
 		}
 	}
 
+	private function storeToDB() {
+		// bulk replace
+		DBQuery::getInstance() -> replace('REPLACE INTO `ItemSuppliers`' . DBUtils::buildMultipleInsert($this -> aStoreData));
+	}
+
 	/**
-	 * @param PlentySoapResponse_GetItemsSuppliers $response
+	 *
+	 *
+	 * @param PlentySoapResponse_GetItemsSuppliers $oPlentySoapResponse_GetItemsSuppliers
 	 */
-	private function responseInterpretation(PlentySoapResponse_GetItemsSuppliers $response) {
-		if (is_array($response -> ItemsSuppliersList -> item)) {
+	private function responseInterpretation(PlentySoapResponse_GetItemsSuppliers $oPlentySoapResponse_GetItemsSuppliers) {
+		if (is_array($oPlentySoapResponse_GetItemsSuppliers -> ItemsSuppliersList -> item)) {
 
-			$countRecords = count($response -> ItemsSuppliersList -> item);
-			$this -> getLogger() -> debug(__FUNCTION__ . " fetched $countRecords supplier records from ItemID: {$response -> ItemsSuppliersList -> item[0]->ItemID} to {$response -> ItemsSuppliersList -> item[$countRecords - 1]->ItemID}");
+			$countRecords = count($oPlentySoapResponse_GetItemsSuppliers -> ItemsSuppliersList -> item);
+			$this -> getLogger() -> debug(__FUNCTION__ . " fetched $countRecords supplier records from ItemID: {$oPlentySoapResponse_GetItemsSuppliers -> ItemsSuppliersList -> item[0]->ItemID} to {$oPlentySoapResponse_GetItemsSuppliers -> ItemsSuppliersList -> item[$countRecords - 1]->ItemID}");
 
-			foreach ($response -> ItemsSuppliersList -> item AS $supplier) {
-				$this -> processSupplier($supplier);
+			foreach ($oPlentySoapResponse_GetItemsSuppliers -> ItemsSuppliersList -> item AS $oPlentySoapObject_ItemsSuppliersList) {
+				$this -> processSupplier($oPlentySoapObject_ItemsSuppliersList);
 			}
 		} else {
 
-			$this -> getLogger() -> debug(__FUNCTION__ . " fetched supplier record from ItemID: {$response -> ItemsSuppliersList -> item -> ItemID}");
+			$this -> getLogger() -> debug(__FUNCTION__ . " fetched supplier record from ItemID: {$oPlentySoapResponse_GetItemsSuppliers -> ItemsSuppliersList -> item -> ItemID}");
 
-			$this -> processSupplier($oPlentySoapResponse_SearchOrders -> Orders -> item);
+			$this -> processSupplier($oPlentySoapResponse_GetItemsSuppliers -> ItemsSuppliersList -> item);
 		}
 	}
-
-	/*
-	 *  [20] => stdClass Object
-	 (
-	 [ItemsSuppliers] => stdClass Object
-	 (
-	 [item] => Array
-	 (
-	 [0] => stdClass Object
-	 (
-	 [ItemSupplierRowID] => 2215
-	 [ItemID] => 885
-	 [SupplierID] => 4
-	 [Priority] => 0
-	 [ItemSupplierPrice] =>
-	 [SupplierMinimumPurchase] => 1
-	 [SupplierItemNumber] => SCT15XA4PHC9003
-	 [SupplierDeliveryTime] => 7
-	 [LastUpdate] => 2012
-	 [Rebate] => 0
-	 [IsRebateAllowed] => 0
-	 [VPE] => 1
-	 )
-
-	 )
-
-	 )
-
-	 [ItemID] => 885
-	 )
-	 * */
 
 	/**
 	 * @param $oPlentySoapObject_ItemsSuppliersList PlentySoapObject_ItemsSuppliersList
 	 */
 	private function processSupplier($oPlentySoapObject_ItemsSuppliersList) {
-		foreach ($oPlentySoapObject_ItemsSuppliersList->ItemsSuppliers->item as $supplier) {/* @var $supplier PlentySoapObject_ItemsSuppliers */
+		if (is_array($oPlentySoapObject_ItemsSuppliersList -> ItemsSuppliers -> item)) {
+			foreach ($oPlentySoapObject_ItemsSuppliersList -> ItemsSuppliers -> item as $oPlentySoapObject_ItemsSuppliers) {
+				/* @var $oPlentySoapObject_ItemsSuppliers PlentySoapObject_ItemsSuppliers */
+
+				// sanity check
+				if (!$oPlentySoapObject_ItemsSuppliersList -> ItemID === $oPlentySoapObject_ItemsSuppliers -> ItemID) {
+					$this -> getLogger() -> debug(__FUNCTION__ . " {$oPlentySoapObject_ItemsSuppliersList->ItemID} != {$supplier->ItemID}");
+					die();
+				}
+
+				// prepare for storing
+				$this -> aStoreData[] = (array)$oPlentySoapObject_ItemsSuppliers;
+			}
+		} else {
 			// sanity check
-			if (!$oPlentySoapObject_ItemsSuppliersList -> ItemID === $supplier -> ItemID) {
-				$this -> getLogger() -> debug(__FUNCTION__ . " {$oPlentySoapObject_ItemsSuppliersList->ItemID} != {$supplier->ItemID}");
+			if (!$oPlentySoapObject_ItemsSuppliersList -> ItemID === $oPlentySoapObject_ItemsSuppliersList -> ItemsSuppliers -> item -> ItemID) {
+				$this -> getLogger() -> debug(__FUNCTION__ . " {$oPlentySoapObject_ItemsSuppliersList->ItemID} != {$oPlentySoapObject_ItemsSuppliersList -> ItemsSuppliers -> item -> ItemID}");
 				die();
 			}
 
-			// store
-			$this -> aStoreData[] = "('{$supplier->ItemSupplierRowID}','{$supplier->ItemID}','{$supplier->SupplierID}','{$supplier->Priority}','{$supplier->ItemSupplierPrice}','{$supplier->SupplierMinimumPurchase}','{$supplier->SupplierItemNumber}','{$supplier->SupplierDeliveryTime}','{$supplier->LastUpdate}','{$supplier->Rebate}','{$supplier->IsRebateAllowed}','{$supplier->VPE}')";
+			// prepare for storing
+			$this -> aStoreData[] = (array)$oPlentySoapObject_ItemsSuppliersList -> ItemsSuppliers -> item;
 		}
 	}
 
