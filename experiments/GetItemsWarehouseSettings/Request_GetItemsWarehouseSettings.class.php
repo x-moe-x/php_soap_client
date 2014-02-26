2985124ed29910f7fb11306651de19483108d644
Moritz Gericke <xmoexdev@gmail.com>
2014-02-26 11:17:58 +0100

    refactored for bulk insert added rudimentary phpdoc type hints

--- experiments/GetItemsWarehouseSettings/Request_GetItemsWarehouseSettings.class.php
+++ experiments/GetItemsWarehouseSettings/Request_GetItemsWarehouseSettings.class.php
@@ -1,12 +1,45 @@
 <?php
 class Request_GetItemsWarehouseSettings {
 
-	public function getRequest($SKUList, $Warehouse) {
+	/**
+	 * @var array Array of SKUs
+	 */
+	private $aSKUList;
+
+	/**
+	 * @return Request_GetItemsWarehouseSettings
+	 */
+	public function __construct() {
+		$this -> aSKUList = array();
+	}
+
+	/**
+	 * @param string $sSKU
+	 * @return void
+	 */
+	public function addSKU($sSKU) {
+		if (count($this -> aSKUList) < SoapCall_GetItemsWarehouseSettings::$MAX_SKU_PER_PAGE) {
+			$this -> aSKUList[] = $sSKU;
+		}
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function isFull(){
+		return count($this -> aSKUList) === SoapCall_GetItemsWarehouseSettings::$MAX_SKU_PER_PAGE;
+	}
+
+	/**
+	 * @param int $
+	 * @return PlentySoapRequest_GetItemsWarehouseSettings
+	 */
+	public function getRequest($warehouse) {
 		$oPlentySoapRequest_GetItemsWarehouseSettings = new PlentySoapRequest_GetItemsWarehouseSettings();
 
-		foreach ($SKUList as $SKU) {
+		foreach ($this -> aSKUList as &$SKU) {
 			$oPlentySoapObject_RequestGetItemsWarehouseSettings = new PlentySoapObject_RequestGetItemsWarehouseSettings();
-			$oPlentySoapObject_RequestGetItemsWarehouseSettings -> WarehouseID = $Warehouse;
+			$oPlentySoapObject_RequestGetItemsWarehouseSettings -> WarehouseID = $warehouse;
 			$oPlentySoapObject_RequestGetItemsWarehouseSettings -> SKU = $SKU;
 			$oPlentySoapRequest_GetItemsWarehouseSettings -> ItemsList[] = $oPlentySoapObject_RequestGetItemsWarehouseSettings;
 		}
--- experiments/GetItemsWarehouseSettings/SoapCall_GetItemsWarehouseSettings.class.php
+++ experiments/GetItemsWarehouseSettings/SoapCall_GetItemsWarehouseSettings.class.php
@@ -7,146 +7,105 @@ require_once ROOT . 'includes/SKUHelper.php';
 
 class SoapCall_GetItemsWarehouseSettings extends PlentySoapCall {
 
-	private $page = 0;
-	private $pages = -1;
-	private $startAtPage = 0;
-	private $oPlentySoapRequest_GetItemsWarehouseSettings = null;
+	/**
+	 * @var int
+	 */
+	public static $MAX_SKU_PER_PAGE = 100;
 
-	private $oItemIDAttributeValueSetIDPairs = null;
-	private $oMaxPairs = -1;
+	/**
+	 * @var array
+	 */
+	private $aStorData;
 
-	private $oWarehouseID = 1;
+	/**
+	 * @var int
+	 */
+	private $warehouseID = 1;
 
-	private $MAX_PAIRS_PER_PAGE = 100;
-
+	/**
+	 * @return SoapCall_GetItemsWarehouseSettings
+	 */
 	public function __construct() {
 		parent::__construct(__CLASS__);
+		$this -> aStorData = array();
 	}
 
+	/**
+	 * @return void
+	 */
 	public function execute() {
-		$this -> getLogger() -> debug(__FUNCTION__);
+		try {
+			// get all possible SKUs
+			$oDBResult = DBQuery::getInstance() -> select($this -> getSKUQuery());
 
-		if ($this -> pages == -1)
-			try {
-				$this -> initPairs();
+			// for every 100 SKUs ...
+			for ($page = 0, $maxPages = ceil($oDBResult -> getNumRows() / self::$MAX_SKU_PER_PAGE); $page < $maxPages; $page++) {/** @var int $page  */
 
-				$oRequest_GetItemsWarehouseSettings = new Request_GetItemsWarehouseSettings();
+				// ... prepare a seperate request
+				$oRequest_GetItemsWarehouseSettings = new Request_GetItemsWarehouseSettings($Warehouse);
+				while (!$oRequest_GetItemsWarehouseSettings -> isFull() && $current = $oDBResult -> fetchAssoc()) {
+					$oRequest_GetItemsWarehouseSettings -> addSKU($current['SKU']);
+				}
 
-				$this -> oPlentySoapRequest_GetItemsWarehouseSettings = $oRequest_GetItemsWarehouseSettings -> getRequest($this -> getSKUList(0), $this -> oWarehouseID);
+				// ... then do soap call ...
+				$oPlentySoapResponse_GetItemsWarehouseSettings = $this -> getPlentySoap() -> GetItemsWarehouseSettings($oRequest_GetItemsWarehouseSettings -> getRequest($this -> warehouseID));
 
-				/*
-				 * do soap call
-				 */
-				$response = $this -> getPlentySoap() -> GetItemsWarehouseSettings($this -> oPlentySoapRequest_GetItemsWarehouseSettings);
+				// ... if successful ...
+				if (($oPlentySoapResponse_GetItemsWarehouseSettings -> Success == true)) {
+					$this -> getLogger() -> debug(__FUNCTION__ . ' Request Success');
 
-				if (($response -> Success == true) && isset($response -> ItemList)) {
-
-					$pagesFound = ceil($this -> oMaxPairs / $this -> MAX_PAIRS_PER_PAGE);
-
-					$this -> getLogger() -> debug(__FUNCTION__ . ' Request Success, ' . count($response -> ItemList -> item) . ' warehouse settings found, ' . $pagesFound . ' pages');
-
-					// process response
-					$this -> responseInterpretation($response);
-
-					if ($pagesFound > $this -> page) {
-						$this -> page = $this -> startAtPage + 1;
-						$this -> pages = $pagesFound;
-
-						$this -> executePages();
-					}
-				} else if (($response -> Success == true) && !isset($response -> ItemList)) {
-					$this -> getLogger() -> debug(__FUNCTION__ . ' Request Success but no items available');
+					// ... then process response
+					$this -> responseInterpretation($oPlentySoapResponse_GetItemsWarehouseSettings);
 				} else {
-					if (isset($response -> ResponseMessages -> item) && is_array($response -> ResponseMessages -> item)) {
-						$errorString = '';
-						foreach ($response -> ResponseMessages -> item as $message) {
-							if (isset($message -> ErrorMessages -> item) && is_array($message -> ErrorMessages -> item)) {
-								foreach ($message -> ErrorMessages -> item as $errorMessage) {
-									$errorString .= $errorMessage -> Key . ': ' . $errorMessage -> Value;
-									$errorString .= ', ';
-								}
-							}
-						}
-						$this -> getLogger() -> debug(__FUNCTION__ . ' Request Error: ' . ($errorString != '' ? $errorString : 'unable to retreive error messages'));
-					} else {
-						$this -> getLogger() -> debug(__FUNCTION__ . ' Request Error');
-					}
+
+					// ... otherwise log error and try next request
+					$this -> getLogger() -> debug(__FUNCTION__ . ' Request Error');
 				}
-			} catch(Exception $e) {
-				$this -> onExceptionAction($e);
 			}
-		else {
-			$this -> executePages();
+
+			// when done store all retrieved data to db
+			$this -> storeToDB();
+
+		} catch(Exception $e) {
+			$this -> onExceptionAction($e);
 		}
 	}
 
-	private function executePages() {
-		while ($this -> pages > $this -> page) {
-			$oRequest_GetItemsWarehouseSettings = new Request_GetItemsWarehouseSettings();
-
-			$this -> oPlentySoapRequest_GetItemsWarehouseSettings = $oRequest_GetItemsWarehouseSettings -> getRequest($this -> getSKUList($this -> page), $this -> oWarehouseID);
-
-			try {
-				$response = $this -> getPlentySoap() -> GetItemsWarehouseSettings($this -> oPlentySoapRequest_GetItemsWarehouseSettings);
-
-				if (($response -> Success == true) && isset($response -> ItemList)) {
-
-					$this -> getLogger() -> debug(__FUNCTION__ . ' Request Success, ' . count($response -> ItemList -> item) . ' warehouse settings found, page: ' . $this -> page);
-
-					// process response
-					$this -> responseInterpretation($response);
-				}
-
-				$this -> page++;
-
-			} catch(Exception $e) {
-				$this -> onExceptionAction($e);
-			}
-		}
-	}
-
-	private function getPairQuery() {
+	/**
+	 * @return string SQL-Query to get all pairs of ItemID -> AttributeValueSetID
+	 */
+	private function getSKUQuery() {
 		return 'SELECT
-                ItemsBase.ItemID,
-                CASE WHEN (AttributeValueSets.AttributeValueSetID IS null) THEN
-                    "0"
-                ELSE
-                    AttributeValueSets.AttributeValueSetID
-                END AttributeValueSetID
-                FROM ItemsBase
-                LEFT JOIN AttributeValueSets
-                    ON ItemsBase.ItemID = AttributeValueSets.ItemID';
+CONCAT(
+	ItemsBase.ItemID,
+	\'-0-\',
+    CASE WHEN (AttributeValueSets.AttributeValueSetID IS null) THEN
+        "0"
+    ELSE
+        AttributeValueSets.AttributeValueSetID
+    END
+) AS SKU
+FROM
+	ItemsBase
+LEFT JOIN
+	AttributeValueSets
+ON
+	ItemsBase.ItemID = AttributeValueSets.ItemID';
 	}
 
-	private function initPairs() {
-		// get all ItemID-AttributeValueSetID pairs
-		$this -> oItemIDAttributeValueSetIDPairs = DBQuery::getInstance() -> select($this -> getPairQuery());
-		$this -> oMaxPairs = $this -> oItemIDAttributeValueSetIDPairs -> getNumRows();
-
-		$this -> getLogger() -> debug(__FUNCTION__ . ' itemid-avsi-pairs: ' . $this -> oMaxPairs);
-	}
-
-	private function getSKUList($startPage) {
-		$requestSKUs = array();
-		for ($i = $startPage * $this -> MAX_PAIRS_PER_PAGE; $i < min(($startPage + 1) * $this -> MAX_PAIRS_PER_PAGE, $this -> oMaxPairs); ++$i) {
-			$current = $this -> oItemIDAttributeValueSetIDPairs -> fetchAssoc();
-			$requestSKUs[] = Values2SKU($current['ItemID'], $current['AttributeValueSetID']);
-		}
-		return $requestSKUs;
+	private function storeToDB() {
+		DBQuery::getInstance() -> insert('INSERT INTO `ItemsWarehouseSettings`' . DBUtils::buildMultipleInsert($this -> aStorData));
 	}
 
 	private function processWarehouseSetting($oWarehouseSetting) {
-		list($ItemID, $PriceID, $AttributeValueSetID) = SKU2Values($oWarehouseSetting -> SKU);
+		list($ItemID, , $AttributeValueSetID) = SKU2Values($oWarehouseSetting -> SKU);
 
-		$this -> getLogger() -> info(__FUNCTION__ . ' SKU: ' . $oWarehouseSetting -> SKU . ' ItemID: ' . $ItemID . ' AVSI: ' . $AttributeValueSetID);
-		// store to db
 		// @formatter:off
-		$query = 'REPLACE INTO `ItemsWarehouseSettings` ' . DBUtils::buildInsert(
-			array(
+		$this->aStorData[] = array(
 				'ID'					=>	$oWarehouseSetting->ID,
 				'MaximumStock'			=>	$oWarehouseSetting->MaximumStock,
 				'ReorderLevel'			=>	$oWarehouseSetting->ReorderLevel,
-			/*  'SKU'					=>	$oWarehouseSetting->SKU,	// replace with ItemID in combination with AVSI */
+			/*  'SKU'					=>	$oWarehouseSetting->SKU,	// replaced with ItemID in combination with AVSI */
 				'ItemID'				=>	$ItemID,
 				'AttributeValueSetID'	=>	$AttributeValueSetID,
 			/*
@@ -158,20 +117,25 @@ class SoapCall_GetItemsWarehouseSettings extends PlentySoapCall {
 				'StorageLocationType'	=>	$oWarehouseSetting->StorageLocationType,
 				'WarehouseID'			=>	$oWarehouseSetting->WarehouseID,
 				'Zone'					=>	$oWarehouseSetting->Zone
-			)
 		);
 		// @formatter:on
-
-		DBQuery::getInstance() -> replace($query);
 	}
 
 	private function responseInterpretation($oPlentySoapResponse_GetItemsWarehouseSettings) {
-		if (is_array($oPlentySoapResponse_GetItemsWarehouseSettings -> ItemList -> item)) {
-			foreach ($oPlentySoapResponse_GetItemsWarehouseSettings -> ItemList -> item as $warehouseSetting) {
+		if (isset($oPlentySoapResponse_GetItemsWarehouseSettings -> ItemList) && is_array($oPlentySoapResponse_GetItemsWarehouseSettings -> ItemList -> item)) {
+
+			$countRecords = count($oPlentySoapResponse_GetItemsWarehouseSettings -> ItemList -> item);
+			$this -> getLogger() -> debug(__FUNCTION__ . " fetched $countRecords warehouse setting records from SKU: {$oPlentySoapResponse_GetItemsWarehouseSettings -> ItemList -> item[0] -> SKU} to {$oPlentySoapResponse_GetItemsWarehouseSettings -> ItemList -> item[$countRecords - 1] -> SKU}");
+
+			foreach ($oPlentySoapResponse_GetItemsWarehouseSettings -> ItemList -> item as &$warehouseSetting) {
 				$this -> processWarehouseSetting($warehouseSetting);
 			}
-		} else {
+		} else if (isset($oPlentySoapResponse_GetItemsWarehouseSettings -> ItemList)) {
+			$this -> getLogger() -> debug(__FUNCTION__ . " fetched warehouse setting records for SKU: {$oPlentySoapResponse_GetItemsWarehouseSettings -> ItemList -> item -> SKU}");
+
 			$this -> processWarehouseSetting($oPlentySoapResponse_GetItemsWarehouseSettings -> ItemList -> item);
+		} else {
+			$this -> getLogger() -> debug(__FUNCTION__ . ' fetched no warehouse setting records for current request');
 		}
 		$this -> getLogger() -> debug(__FUNCTION__ . ' done.');
 	}
