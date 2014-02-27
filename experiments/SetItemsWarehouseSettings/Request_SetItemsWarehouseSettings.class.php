75cc0be313b56bfc8526e0ace80248bd7052ac41
x-moe-x <xmoexdev@gmail.com>
2014-02-27 11:11:10 +0100

    filled set items warehouse settings request small corrections

--- experiments/SetItemsWarehouseSettings/Request_SetItemsWarehouseSettings.class.php
+++ experiments/SetItemsWarehouseSettings/Request_SetItemsWarehouseSettings.class.php
@@ -3,10 +3,54 @@
 class Request_SetItemsWarehouseSettings {
 
 	/**
+	 * @var array
+	 */
+	private $aItemsWarehouseSettings;
+
+	/**
+	 * @param array $aWarehouseSetting
+	 * @return void
+	 */
+	public function addItemsWarehouseSetting(array $aWarehouseSetting) {
+		if (count($this -> aItemsWarehouseSettings) < SoapCall_SetItemsWarehouseSettings::$MAX_WAREHOUSE_SETTINGS_PER_PAGE) {
+			$this -> aItemsWarehouseSettings[] = $aWarehouseSetting;
+		}
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function isFull() {
+		return count($this -> aItemsWarehouseSettings) === SoapCall_SetItemsWarehouseSettings::$MAX_WAREHOUSE_SETTINGS_PER_PAGE;
+	}
+
+	/**
+	 * @return Request_SetItemsWarehouseSettings
+	 */
+	public function __construct() {
+		$this -> aItemsWarehouseSettings = array();
+	}
+
+	/**
+	 * @param int $warehouse
 	 * @return PlentySoapRequest_SetItemsWarehouseSettings
 	 */
-	public function getRequest() {
+	public function getRequest($warehouse) {
 		$oPlentySoapRequest_SetItemsWarehouseSettings = new PlentySoapRequest_SetItemsWarehouseSettings();
+
+		$oPlentySoapRequest_SetItemsWarehouseSettings -> ItemsList = new ArrayOfPlentysoapobject_setitemswarehousesettings();
+		$oPlentySoapRequest_SetItemsWarehouseSettings -> VariantSettings = FALSE;
+
+		$oPlentySoapRequest_SetItemsWarehouseSettings -> ItemsList -> item = array();
+
+		foreach ($this->aItemsWarehouseSettings as $aItemsWarehouseSetting) {
+			$oPlentySoapObject_SetItemsWarehouseSettings = new PlentySoapObject_SetItemsWarehouseSettings();
+
+			fillObjectFromArray($oPlentySoapObject_SetItemsWarehouseSettings, $aItemsWarehouseSetting, array('WarehouseID' => $warehouse));
+			
+			$oPlentySoapRequest_SetItemsWarehouseSettings -> ItemsList -> item[] = $oPlentySoapObject_SetItemsWarehouseSettings;
+
+		}
 
 		return $oPlentySoapRequest_SetItemsWarehouseSettings;
 	}
--- experiments/SetItemsWarehouseSettings/SoapCall_SetItemsWarehouseSettings.class.php
+++ experiments/SetItemsWarehouseSettings/SoapCall_SetItemsWarehouseSettings.class.php
@@ -4,6 +4,8 @@ require_once ROOT . 'lib/soap/call/PlentySoapCall.abstract.php';
 require_once 'Request_SetItemsWarehouseSettings.class.php';
 
 class SoapCall_SetItemsWarehouseSettings extends PlentySoapCall {
+	
+	public static $MAX_WAREHOUSE_SETTINGS_PER_PAGE = 100;
 
 	/**
 	 * @return SoapCall_SetItemsWarehouseSettings
