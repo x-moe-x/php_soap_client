799567395d4cb30ea4450ec5cbb4938dabea2442
x-moe-x <xmoexdev@gmail.com>
2014-03-05 11:07:09 +0100

    streamlined calculate histogram added verbose phpdoc comments

--- experiments/CalculateHistogram/CalculateHistogram.class.php
+++ experiments/CalculateHistogram/CalculateHistogram.class.php
@@ -4,31 +4,45 @@ require_once ROOT . 'lib/db/DBQuery.class.php';
 require_once ROOT . 'lib/db/DBQueryResult.class.php';
 require_once ROOT . 'includes/SKUHelper.php';
 require_once ROOT . 'includes/GetConfig.php';
+require_once ROOT . 'includes/DBUtils2.class.php';
 
 /**
  * @author x-moe-x
  * @copyright net-xpress GmbH & Co. KG www.net-xpress.com
  */
 class CalculateHistogram {
+
 	/**
-	 *
 	 * @var string
 	 */
 	private $identifier4Logger = '';
 
-	private $config = null;
+	/**
+	 * @var array
+	 */
+	private $aConfig;
 
-	private $currentTime = null;
+	/**
+	 * @var int
+	 */
+	private $currentTime;
 
+	/**
+	 * @var array
+	 */
+	private $aArticleData;
+
+	/**
+	 * @return CalculateHistogram
+	 */
 	public function __construct() {
 		$this -> identifier4Logger = __CLASS__;
-	}
 
-	public function init() {
 		$this -> currentTime = time();
-		//  30.04.2013, 23:59:59 = 1367359199;
 
-		$this -> config = Config::getAll();
+		$this -> aArticleData = array();
+
+		$this -> aConfig = Config::getAll();
 
 		// set group_concat_max_len to reasonable value to prevent cropping of article quantities list
 		DBQuery::getInstance() -> Set('SET SESSION group_concat_max_len = 4096');
@@ -37,66 +51,73 @@ class CalculateHistogram {
 		DBQuery::getInstance() -> truncate('TRUNCATE TABLE CalculatedDailyNeeds');
 	}
 
+	/**
+	 * calculate daily need for every article
+	 *
+	 * @return void
+	 */
 	public function execute() {
 
-		$this -> init();
-
 		// retrieve latest orders from db for calculation time a
-		$articleResultA = DBQuery::getInstance() -> select($this -> getIntervallQuery($this -> config['CalculationTimeA']['Value']));
+		$articleResultA = DBQuery::getInstance() -> select($this -> getIntervalQuery($this -> aConfig['CalculationTimeA']['Value']));
 		$this -> getLogger() -> info(__FUNCTION__ . ' : retrieved ' . $articleResultA -> getNumRows() . ' article variants for calculation time a');
 
-		$articleData = array();
 		// calculate data for calculation time a
-		while ($currentArticle = $articleResultA -> fetchAssoc()) {
-			$this -> processArticle($articleData, $currentArticle, 'A');
+		while ($aCurrentArticle = $articleResultA -> fetchAssoc()) {
+			$this -> processArticle($aCurrentArticle, 'A');
 		}
 
-		// retreive latest orders from db for calculation time b
-		$articleResultB = DBQuery::getInstance() -> select($this -> getIntervallQuery($this -> config['CalculationTimeB']['Value']));
+		// retrieve latest orders from db for calculation time b
+		$articleResultB = DBQuery::getInstance() -> select($this -> getIntervalQuery($this -> aConfig['CalculationTimeB']['Value']));
 		$this -> getLogger() -> info(__FUNCTION__ . ' : retrieved ' . $articleResultB -> getNumRows() . ' article variants for calculation time b');
+
 		// for every article in calculation time b do:
 		// combine a and b
-		while ($currentArticle = $articleResultB -> fetchAssoc()) {
-			$this -> processArticle($articleData, $currentArticle, 'B');
+		while ($aCurrentArticle = $articleResultB -> fetchAssoc()) {
+			$this -> processArticle($aCurrentArticle, 'B');
 		}
 
-		$this -> storeToDB($articleData);
+		$this -> storeToDB();
 	}
 
-	private function storeToDB($articleData) {
-		$result = array();
-		foreach ($articleData as $sku => $current) {
-			$result[] = "('{$current['ItemID']}','{$current['AttributeValueSetID']}','{$current['DailyNeed']}','{$current['LastUpdate']}','{$current['QuantitiesA']}','{$current['SkippedA']}','{$current['QuantitiesB']}','{$current['SkippedB']}')";
-		}
-
-		$query = 'REPLACE INTO `CalculatedDailyNeeds` (ItemID,AttributeValueSetID,DailyNeed,LastUpdate,QuantitiesA,SkippedA,QuantitiesB,SkippedB) VALUES' . implode(',', $result);
-		DBQuery::getInstance() -> replace($query);
+	/**
+	 * store article data to db
+	 *
+	 * @return void
+	 */
+	private function storeToDB() {
+		DBQuery::getInstance() -> insert('INSERT INTO `CalculatedDailyNeeds`' . DBUtils2::buildMultipleInsertOnDuplikateKeyUpdate($this -> aArticleData));
 	}
 
-	private function processArticle(&$articleData, $currentArticle, $AorB) {
-		list($ItemID, $PriceID, $AttributeValueSetID) = SKU2Values($currentArticle['SKU']);
-		$AorB = strtoupper($AorB);
-		if ($AorB !== 'A' && $AorB !== 'B') {
-			$this -> getLogger() -> info(__FUNCTION__ . ' : wrong syntax of $AorB : ' . $AorB);
+	/**
+	 * calculate spike cleared daily need and additional raw-data for current article combined from both calculation times as follows: dailyNeed = (dailyNeedA + dailyNeedB) / 2
+	 *
+	 * @param array $aCurrentArticle associative array of current article
+	 * @param string $sAorB select calculation time a or b
+	 * @return void
+	 */
+	private function processArticle(array $aCurrentArticle, $sAorB) {
+		list($ItemID, , $AttributeValueSetID) = SKU2Values($aCurrentArticle['SKU']);
+		$sAorB = strtoupper($sAorB);
+		if ($sAorB !== 'A' && $sAorB !== 'B') {
+			$this -> getLogger() -> info(__FUNCTION__ . ' : wrong syntax of $sAorB : ' . $sAorB);
 			die();
 		}
 
 		$skippedIndex;
-		$quantities = explode(',', $currentArticle['quantities']);
-		$minToleratedSpikes = $this -> config['MinimumToleratedSpikes' . $AorB]['Value'];
-		$minOrders = $this -> config['MinimumOrders' . $AorB]['Value'];
-		$adjustedQuantity = $this -> getArticleAdjustedQuantity($quantities, $currentArticle['quantity'], $currentArticle['range'], $minToleratedSpikes, $minOrders, $skippedIndex);
+		$adjustedQuantity = $this -> getArticleAdjustedQuantity(explode(',', $aCurrentArticle['quantities']), $aCurrentArticle['quantity'], $aCurrentArticle['range'], $this -> aConfig['MinimumToleratedSpikes' . $sAorB]['Value'], $this -> aConfig['MinimumOrders' . $sAorB]['Value'], $skippedIndex);
 
-		if ($AorB === 'A') {
+		if ($sAorB === 'A') {
 			// add data for calculation time A
+
 			// @formatter:off
-			$articleData[$currentArticle['SKU']] = 
+			$this->aArticleData[$aCurrentArticle['SKU']] = 
 				array(
 					'ItemID' => 				$ItemID,
 					'AttributeValueSetID' =>	$AttributeValueSetID,
-					'DailyNeed' =>				($adjustedQuantity / $this -> config['CalculationTimeA']['Value'])/2,
+					'DailyNeed' =>				($adjustedQuantity / $this -> aConfig['CalculationTimeA']['Value'])/2,
 					'LastUpdate' =>				$this -> currentTime,
-					'QuantitiesA' =>			$currentArticle['quantities'],
+					'QuantitiesA' =>			$aCurrentArticle['quantities'],
 					'SkippedA' =>				$skippedIndex,
 					'QuantitiesB' =>			0,
 					'SkippedB' => 				0
@@ -104,51 +125,76 @@ class CalculateHistogram {
 			// @formatter:on
 		} else {
 			// add data for calculation time B
-			if (array_key_exists($currentArticle['SKU'], $articleData)) {
-				// use existing record
-				$articleData[$currentArticle['SKU']]['DailyNeed'] = ($adjustedQuantity / $this -> config['CalculationTimeB']['Value']) / 2 + $articleData[$currentArticle['SKU']]['DailyNeed'];
-				$articleData[$currentArticle['SKU']]['QuantitiesB'] = $currentArticle['quantities'];
-				$articleData[$currentArticle['SKU']]['SkippedB'] = $skippedIndex;
+
+			// if there's an existing record ...
+			if (array_key_exists($aCurrentArticle['SKU'], $this -> aArticleData)) {
+				// ... then use existing record
+				$this -> aArticleData[$aCurrentArticle['SKU']]['DailyNeed'] = ($adjustedQuantity / $this -> aConfig['CalculationTimeB']['Value']) / 2 + $this -> aArticleData[$aCurrentArticle['SKU']]['DailyNeed'];
+				$this -> aArticleData[$aCurrentArticle['SKU']]['QuantitiesB'] = $aCurrentArticle['quantities'];
+				$this -> aArticleData[$aCurrentArticle['SKU']]['SkippedB'] = $skippedIndex;
 			} else {
-				// create new one
-				array('ItemID' => $ItemID, 'AttributeValueSetID' => $AttributeValueSetID, 'DailyNeed' => ($adjustedQuantity / $this -> config['CalculationTimeB']['Value']) / 2, 'LastUpdate' => $this -> currentTime, 'QuantitiesA' => 0, 'SkippedA' => 0, 'QuantitiesB' => $currentArticle['quantities'], 'SkippedB' => $skippedIndex);
+				// ... otherwise create new one
+
+				// @formatter:off
+				array(
+					'ItemID' =>					$ItemID,
+					'AttributeValueSetID' =>	$AttributeValueSetID,
+					'DailyNeed' =>				($adjustedQuantity / $this -> aConfig['CalculationTimeB']['Value']) / 2,
+					'LastUpdate' =>				$this -> currentTime,
+					'QuantitiesA' =>			0,
+					'SkippedA' =>				0,
+					'QuantitiesB' =>			$aCurrentArticle['quantities'],
+					'SkippedB' =>				$skippedIndex
+				);
+				// @formatter:on
 			}
 		}
 	}
 
-	private function getArticleAdjustedQuantity($quantities, $quantity, $range, $minToleratedSpikes, $minOrders, &$index) {
-
-		$spikeTolerance = $this -> config['SpikeTolerance']['Value'];
-
+	/**
+	 * compute an adjusted total quantity for given quantities (current article) wich is cleared of untolerated spikes
+	 *
+	 * @param array $aQuantities Array of quantities for current article to discard all spikes from
+	 * @param int $quantity total quantity of the current article
+	 * @param int $range range for quantities, quantities above this range have to be checked for tolerated spikes
+	 * @param int $minToleratedSpikes minimum # of spikes that can be tolerated
+	 * @param int $minOrders minimum # of orders necessary to consider the current article
+	 * @param int $index return value for the # of skipped orders
+	 * @return total quantity minus discarded spikes
+	 */
+	private function getArticleAdjustedQuantity(array $aQuantities, $quantity, $range, $minToleratedSpikes, $minOrders, &$index) {
 		// skip all orders if # of orders is below given minimum ...
-		if (count($quantities) < $minOrders) {
-			$index = count($quantities);
+		if (count($aQuantities) < $minOrders) {
+			$index = count($aQuantities);
 			return 0;
 		}
 
-		// ... otherwise check quantities in descending order
-		for ($index = 0; $index < count($quantities); ++$index) {
+		$spikeTolerance = Config::get('SpikeTolerance');
 
-			// if we are already below the confidence range: stop the loop
-			if ($quantities[$index] <= $range) {
+		// ... otherwise check quantities in descending order
+		for ($index = 0, $maxQuantities = count($aQuantities); $index < $maxQuantities; ++$index) {
+
+			// if we are already below the confidence range ...
+			if ($aQuantities[$index] <= $range) {
+				// ... then stop the loop
 				break;
 			} else {
-				// otherwise we need to check for tolerated spikes
+				// ... otherwise we need to check for tolerated spikes
 
 				// get sub array
-				$spikes = array_slice($quantities, $index, $minToleratedSpikes);
+				$aSpikes = array_slice($aQuantities, $index, $minToleratedSpikes);
 
 				// assume all spikes are in tolerance range
 				$tolerateSpikes = true;
 
 				// check subarray
-				for ($spikeIndex = 1; $spikeIndex < count($spikes); ++$spikeIndex) {
+				for ($spikeIndex = 1, $maxSpikes = count($aSpikes); $spikeIndex < $maxSpikes; ++$spikeIndex) {
 
-					// if at least one element is below spike tolerance range:
-					if ($spikes[$spikeIndex] < $spikes[0] * (1 - $spikeTolerance)) {
+					// if at least one element is below spike tolerance range ...
+					if ($aSpikes[$spikeIndex] < $aSpikes[0] * (1 - $spikeTolerance)) {
 
-						// skip normative spike and break off the loop to try the next one...
-						$quantity -= $quantities[$index];
+						// ... then skip spike and break off the loop to try the next one...
+						$quantity -= $aQuantities[$index];
 						$tolerateSpikes = false;
 						break;
 					}
@@ -163,16 +209,18 @@ class CalculateHistogram {
 		return $quantity;
 	}
 
-	private function getIntervallQuery($daysBack) {
-		$startTimestamp = $this -> currentTime;
-		$rangeConfidenceMultiplyer = $this -> config['StandardDeviationFactor']['Value'];
-
-		// @formatter:off
+	/**
+	 * prepare query to get total quantities and spike-toleration-data for all articles from db
+	 *
+	 * @param int $daysBack
+	 * @return string query
+	 */
+	private function getIntervalQuery($daysBack) {
 		return "SELECT
 	OrderItem.ItemID,
 	OrderItem.SKU,
 	SUM(CAST(OrderItem.Quantity AS SIGNED)) AS `quantity`,
-	AVG(`quantity`) + STDDEV(`quantity`) * $rangeConfidenceMultiplyer AS `range`,
+	AVG(`quantity`) + STDDEV(`quantity`) * {$this->aConfig['StandardDeviationFactor']['Value']} AS `range`,
 	CAST(GROUP_CONCAT(IF(OrderItem.Quantity > 0 ,CAST(OrderItem.Quantity AS SIGNED),NULL) ORDER BY OrderItem.Quantity DESC SEPARATOR \",\") AS CHAR) AS `quantities`,
 	ItemsBase.Marking1ID
 FROM
@@ -180,15 +228,14 @@ FROM
 LEFT JOIN
 	(OrderHead, ItemsBase) ON (OrderHead.OrderID = OrderItem.OrderID AND OrderItem.ItemID = ItemsBase.ItemID)
 WHERE
-	(OrderHead.OrderTimestamp BETWEEN $startTimestamp -( 86400 *  $daysBack ) AND $startTimestamp ) AND
+	(OrderHead.OrderTimestamp BETWEEN {$this -> currentTime} -( 86400 *  $daysBack ) AND {$this -> currentTime} ) AND
 	(OrderHead.OrderStatus < 8 OR OrderHead.OrderStatus >= 9) AND
 	OrderType = \"order\" AND
 	ItemsBase.Marking1ID IN (9,12,16,20) /* yellow, red, green, black */
 GROUP BY
 	OrderItem.SKU
 ORDER BY
-	ItemID".PHP_EOL;
-		// @formatter:on
+	ItemID" . PHP_EOL;
 	}
 
 	/**
