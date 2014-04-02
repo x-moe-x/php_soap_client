5cc01a536d78c52c953bdcda8d2a80f7c91e5f5d
x-moe-x <xmoexdev@gmail.com>
2014-04-02 14:48:26 +0200

    added call skeleton

---
+++ experiments/GetItemsPriceLists/Request_GetItemsPriceLists.class.php
@@ -0,0 +1,15 @@
+<?php
+
+class Request_GetItemsPriceLists {
+
+	/**
+	 * @return PlentySoapRequest_GetItemsPriceLists
+	 */
+	public function getRequest() {
+		$oPlentySoapRequest_GetItemsPriceLists = new PlentySoapRequest_GetItemsPriceLists();
+
+		return $oPlentySoapRequest_GetItemsPriceLists;
+	}
+
+}
+?>
---
+++ experiments/GetItemsPriceLists/SoapCall_GetItemsPriceLists.class.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once ROOT . 'lib/soap/call/PlentySoapCall.abstract.php';
+require_once 'Request_GetItemsPriceLists.class.php';
+
+class SoapCall_GetItemsPriceLists extends PlentySoapCall {
+
+	/**
+	 * @return SoapCall_GetItemsPriceLists
+	 */
+	public function __construct() {
+		parent::__construct(__CLASS__);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function execute() {
+		try {
+
+			$oRequest_GetItemsPriceLists = new Request_GetItemsPriceLists();
+
+			/*
+			 * do soap call
+			 */
+			$response = $this -> getPlentySoap() -> GetItemsPriceLists($oRequest_GetItemsPriceLists -> getRequest());
+
+			if ($response -> Success == true) {
+				// request successful, processing data..
+				$this -> getLogger() -> debug(__FUNCTION__ . ' Request Success');
+			} else {
+				$this -> getLogger() -> debug(__FUNCTION__ . ' Request Error');
+			}
+		} catch(Exception $e) {
+			$this -> onExceptionAction($e);
+		}
+	}
+
+}
+?>
