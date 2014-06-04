c424083e7e46feb69bdf0fd0223081f1e62a3df8
Moritz Gericke <xmoexdev@gmail.com>
2014-06-04 16:49:46 +0200

    fixed annoying bug

--- webinterface/price-post-xml.php
+++ webinterface/price-post-xml.php
@@ -5,8 +5,8 @@ require_once ROOT . 'api/ApiAmazon.class.php';
 
 $page = isset($_POST['page']) ? $_POST['page'] : 1;
 $rp = isset($_POST['rp']) ? $_POST['rp'] : 10;
-$sortname = isset($_POST['sortname']) ? $_POST['sortname'] : 'ItemID';
-$sortorder = isset($_POST['sortorder']) ? $_POST['sortorder'] : 'ASC';
+$sortname = isset($_POST['sortname']) && !empty($_POST['sortname']) ? $_POST['sortname'] : 'ItemID';
+$sortorder = isset($_POST['sortorder']) && !empty($_POST['sortorder']) ? $_POST['sortorder'] : 'ASC';
 $query = isset($_POST['query']) ? $_POST['query'] : false;
 $qtype = isset($_POST['qtype']) ? $_POST['qtype'] : false;
 
