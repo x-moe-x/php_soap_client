423b501725ce4b78ed2426d301cfda4d85a6ce93
Moritz Gericke <xmoexdev@gmail.com>
2014-06-18 12:34:19 +0200

    implemented incremental request buildup functionality to set price sets request generator

--- experiments/SetPriceSets/Request_SetPriceSets.class.php
+++ experiments/SetPriceSets/Request_SetPriceSets.class.php
@@ -1,12 +1,55 @@
 <?php
 
+require_once ROOT . 'includes/FillObjectFromArray.php';
+
 class Request_SetPriceSets {
+
+	/**
+	 * @var array
+	 */
+	private $aPriceSets;
+
+	/**
+	 * @return Request_SetPriceSets
+	 */
+	public function __construct() {
+		$this -> aPriceSets = array();
+	}
+
+	/**
+	 * @param array $aPriceSet
+	 * @return void
+	 */
+	public function addPriceSet(array $aPriceSet) {
+		if (count($this -> aPriceSets) < SoapCall_SetPriceSets::MAX_PRICE_SETS_PER_PAGE) {
+			$this -> aPriceSets[] = $aPriceSet;
+		}
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function isFull() {
+		return count($this -> aPriceSets) === SoapCall_SetPriceSets::MAX_PRICE_SETS_PER_PAGE;
+	}
 
 	/**
 	 * @return PlentySoapRequest_SetPriceSets
 	 */
 	public function getRequest() {
-		return null;
+		$oPlentySoapRequest_SetPriceSets = new PlentySoapRequest_SetPriceSets();
+		$oPlentySoapRequest_SetPriceSets -> PriceSetList = array();
+
+		foreach ($this->aPriceSets as &$aPriceSet) {/* @var $aPriceSet array */
+
+			$oPlentySoapObject_SetPriceSets = new PlentySoapObject_SetPriceSets();
+
+			fillObjectFromArray($oPlentySoapObject_SetPriceSets, $aPriceSet);
+
+			$oPlentySoapRequest_SetPriceSets -> PriceSetList[] = $oPlentySoapObject_SetPriceSets;
+		}
+
+		return $oPlentySoapRequest_SetPriceSets;
 	}
 
 }
