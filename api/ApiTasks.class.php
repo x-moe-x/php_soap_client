3deb6b2aac1706456b5fa1f4ea8c021af8a5bf8a
xmoexdev <xmoexdev@gmail.com>
2015-01-29 13:11:58 +0100

    added lots of comments

--- api/ApiTasks.class.php
+++ api/ApiTasks.class.php
@@ -2,8 +2,11 @@
 require_once realpath(dirname(__FILE__) . '/../') . '/config/basic.inc.php';
 require_once ROOT . 'api/ApiHelper.class.php';
 
-class ApiTasks {
-
+/**
+ * Provides methods to manage tasks and the task queue
+ */
+class ApiTasks
+{
 	/**
 	 * @var int
 	 */
@@ -15,25 +18,28 @@ class ApiTasks {
 	const EXECUTION_ESTIMATE_SECONDS = 30;
 
 	/**
-	 * @param int $id TaskID to be updated
+	 * Updates the tasks execution timestamp
+	 *
+	 * @param int $id id of to be updated task
+	 *
 	 * @return void
 	 */
-	public static function updateLastExecuteTime($id) {
-		ob_start();
+	public static function updateLastExecuteTime($id)
+	{
 		$now = time();
-		DBQuery::getInstance() -> insert("INSERT INTO
-	`TaskData`
-	(`TaskID`, `TaskLastExecutionTimestamp`)
-	VALUES($id, $now)
-ON DUPLICATE KEY UPDATE
-	`TaskLastExecutionTimestamp` = $now");
+
+		ob_start();
+		DBQuery::getInstance()->insert("INSERT INTO `TaskData` (`TaskID`, `TaskLastExecutionTimestamp`)	VALUES($id, $now) ON DUPLICATE KEY UPDATE `TaskLastExecutionTimestamp` = $now");
 		ob_end_clean();
 	}
 
 	/**
-	 * @return array Array of all tasks, scheduled for immediate execution
+	 * Checks database for tasks that are scheduled for immediate execution
+	 *
+	 * @return array Array tasks scheduled for execution
 	 */
-	public static function getScheduledTasks() {
+	public static function getScheduledTasks()
+	{
 		ob_start();
 		$dbQueryResult = DBQuery::getInstance() -> select('SELECT
 	def.TaskID as `id`,
@@ -57,43 +63,48 @@ OR
 		$now = new DateTime();
 
 		// for every existing task...
-		while ($currentTask = $dbQueryResult -> fetchAssoc()) {
+		while ($currentTask = $dbQueryResult->fetchAssoc())
+		{
 			// ... prepare next execution base
 			$next = new DateTime('@' . $currentTask['lastExecution']);
-			$next -> setTimeZone(new DateTimeZone('Europe/Berlin'));
+			$next->setTimeZone(new DateTimeZone('Europe/Berlin'));
 
 			// ... if task is daily task ...
-			if (isset($currentTask['start'])) {
-				$currentMinutes = intval($now -> format('H')) * 60 + intval($now -> format('i'));
+			if (isset($currentTask['start']))
+			{
+				$currentMinutes = intval($now->format('H')) * 60 + intval($now->format('i'));
 				$startTime = intval($currentTask['start']);
 
 				// ... then check if start time is reached (within confidence intervall) ...
-				if ($startTime <= $currentMinutes && $currentMinutes < $startTime + 2 * self::DAEMON_INTERVAL) {
+				if ($startTime <= $currentMinutes && $currentMinutes < $startTime + 2 * self::DAEMON_INTERVAL)
+				{
 					// ... then adjust next execution time (so no daily task is scheduled accidentally twice or more that day)
-					$next -> add(new DateInterval('PT' . (2 * self::DAEMON_INTERVAL) . 'M'));
-				}
-				// ... or if start time isn't reached ...
-				else {
+					$next->add(new DateInterval('PT' . (2 * self::DAEMON_INTERVAL) . 'M'));
+				} // ... or if start time isn't reached ...
+				else
+				{
 					// ... then do nothing
 					continue;
 				}
-			}
-			// ... or if task is periodic task
-			else {
+			} // ... or if task is periodic task
+			else
+			{
 				// ... then adjust next execution time
-				$next -> add(new DateInterval('PT' . $currentTask['interval'] . 'M')) -> sub(new DateInterval('PT' . self::EXECUTION_ESTIMATE_SECONDS . 'S'));
+				$next->add(new DateInterval('PT' . $currentTask['interval'] . 'M'))->sub(new DateInterval('PT' . self::EXECUTION_ESTIMATE_SECONDS . 'S'));
 			}
 
 			// ... then check if execution is scheduled
-			if ($next < $now) {
+			if ($next < $now)
+			{
 				// ... enqueue task
 				$result[] = $currentTask;
-			}
-			// ... or if execution isn't scheduled
-			else {
+			} // ... or if execution isn't scheduled
+			else
+			{
 				// ... then do nothing
 			}
 		}
+
 		return $result;
 	}
 
