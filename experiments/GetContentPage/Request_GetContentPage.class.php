6527557d72fb7324ea8627a73b1332ab90c589de
xmoexdev <xmoexdev@gmail.com>
2015-02-04 13:37:25 +0100

    custom request class

--- experiments/GetContentPage/Request_GetContentPage.class.php
+++ experiments/GetContentPage/Request_GetContentPage.class.php
@@ -1,22 +1,28 @@
 <?php
-class Request_GetContentPage {
+
+class Request_GetContentPage
+{
 
 	/**
 	 * Prepares a request for a specific content page in a specific language
 	 *
-	 * @param int $contentPageID the content page's numeric id
-	 * @param string $language the language's shortcut
+	 * @param int    $contentPageID the content page's numeric id
+	 * @param int    $storeID       the store's numeric id
+	 * @param string $language      the language's shortcut
 	 *
 	 * @return PlentySoapRequest_GetContentPage
 	 */
-	public static function getRequest($contentPageID, $language = 'de') {
+	public static function getRequest($contentPageID, $storeID = 0, $language = 'de')
+	{
 		$oPlentySoapRequest_GetContentPage = new PlentySoapRequest_GetContentPage();
 
-		$oPlentySoapRequest_GetContentPage -> ContentPageID = $contentPageID;
-		$oPlentySoapRequest_GetContentPage -> Lang = $language;
+		$oPlentySoapRequest_GetContentPage->ContentPageID = $contentPageID;
+		$oPlentySoapRequest_GetContentPage->Lang = $language;
+		$oPlentySoapRequest_GetContentPage->StoreID = $storeID;
 
 		return $oPlentySoapRequest_GetContentPage;
 	}
 
 }
+
 ?>
