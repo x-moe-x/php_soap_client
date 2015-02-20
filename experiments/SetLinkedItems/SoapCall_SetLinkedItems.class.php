12093fb69d72f4accce3d3fb6e654124acfb79f9
xmoexdev <xmoexdev@gmail.com>
2015-02-20 16:06:42 +0100

    implemented SetLinkedItems

--- experiments/SetLinkedItems/SoapCall_SetLinkedItems.class.php
+++ experiments/SetLinkedItems/SoapCall_SetLinkedItems.class.php
@@ -23,26 +23,56 @@ class SoapCall_SetLinkedItems extends PlentySoapCall
 	{
 		try
 		{
-			// prepare request
-			$preparedRequest = new RequestContainer_SetLinkedItems(12, 'similar');
+			// select every item from db
+			$setLinkedItemsDbResult = DBQuery::getInstance()->select($this->getQuery());
 
-			// add items
-			$preparedRequest->add(15,1);
-
-			/** @var PlentySoapResponse_SetLinkedItems $response */
-			$response = $this->getPlentySoap()->SetLinkedItems($preparedRequest->getRequest());
-
-
-			if ($response->Success == true)
+			// for every main item and relationship ...
+			while ($row = $setLinkedItemsDbResult->fetchAssoc())
 			{
-			} else
-			{
-				// ... otherwise log error and try next request
-				$this->debug(__FUNCTION__ . ' Request Error');
+				// ... prepare a separate request ...
+				$preparedRequest = new RequestContainer_SetLinkedItems($row['ItemID'], $row['Relationship']);
+
+				// ... add linked items ...
+				$linkedItems = explode(',', $row['LinkedItemIDList']);
+				foreach ($linkedItems as &$linkedItem)
+				{
+					$preparedRequest->add($linkedItem);
+				}
+
+				// ... then do soap call
+				/** @var PlentySoapResponse_SetLinkedItems $response */
+				$response = $this->getPlentySoap()->SetLinkedItems($preparedRequest->getRequest());
+
+				if ($response->Success == true)
+				{
+					$this->debug(__FUNCTION__ . ' successfully updated Item ' . $row['ItemID'] . '\'s linked items');
+				} else
+				{
+					// ... otherwise log error and try next request
+					$this->debug(__FUNCTION__ . ' Request Error for Item ' . $row['ItemID']);
+				}
 			}
 		} catch (Exception $e)
 		{
 			$this->onExceptionAction($e);
 		}
+	}
+
+	/**
+	 * @return string
+	 */
+	private function getQuery()
+	{
+		return 'SELECT
+  ItemID,
+  CAST(
+      GROUP_CONCAT(
+          CAST(LinkedItemID AS SIGNED) ORDER BY LinkedItemID ASC
+          SEPARATOR ",") AS CHAR) AS LinkedItemIDList,
+  RelationShip
+FROM `SetLinkedItems`
+GROUP BY
+  ItemID,
+  RelationSHip';
 	}
 }
