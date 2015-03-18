ed9ebdeafc1de065e462a2ef88c819615f8ab82e
xmoexdev <xmoexdev@gmail.com>
2015-03-18 10:49:02 +0100

    added basic class skeletons

---
+++ experiments/SetItemsTexts/RequestContainer_SetItemsTexts.class.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once ROOT . 'includes/RequestContainer.class.php';
+
+/**
+ * Class RequestContainer_SetItemsTexts
+ */
+class RequestContainer_SetItemsTexts extends RequestContainer
+{
+
+	/**
+	 * returns the assembled request
+	 *
+	 * @return mixed
+	 */
+	public function getRequest()
+	{
+		// TODO: Implement getRequest() method.
+	}
+}
---
+++ experiments/SetItemsTexts/SoapCall_SetItemsTexts.class.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once ROOT . 'lib/soap/call/PlentySoapCall.abstract.php';
+
+/**
+ * Class SoapCall_SetItemsTexts
+ */
+class SoapCall_SetItemsTexts extends PlentySoapCall
+{
+	/**
+	 * @var int
+	 */
+	const MAX_ITEMS_PER_PAGES = 50;
+
+	public function execute()
+	{
+	}
+}
