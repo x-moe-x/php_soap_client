d48f28b0db0b383624c009295657da0041c77328
xmoexdev <xmoexdev@gmail.com>
2015-04-22 12:18:42 +0200

    added basic class skeletons

---
+++ experiments/SetItemsFreeTextFields/RequestContainer_SetItemsFreeTextFields.class.php
@@ -0,0 +1,18 @@
+<?php
+require_once ROOT . 'includes/FillObjectFromArray.php';
+require_once ROOT . 'includes/RequestContainer.class.php';
+
+/**
+ * Class RequestContainer_SetItemsFreeTextFields
+ */
+class RequestContainer_SetItemsFreeTextFields extends RequestContainer {
+
+	/**
+	 * returns the assembled request
+	 *
+	 * @return mixed
+	 */
+	public function getRequest() {
+		// TODO: Implement getRequest() method.
+	}
+}
---
+++ experiments/SetItemsFreeTextFields/SoapCall_SetItemsFreeTextFields.class.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once ROOT . 'lib/soap/call/PlentySoapCall.abstract.php';
+require_once 'RequestContainer_SetItemsFreeTextFields.class.php';
+
+/**
+ * Class SoapCall_SetItemsFreeTextFields
+ */
+class SoapCall_SetItemsFreeTextFields extends PlentySoapCall {
+	/**
+	 * @var int
+	 */
+	const MAX_ITEMS_PER_PAGES = 100;
+
+	public function execute() {
+		// TODO: Implement execute() method.
+	}
+}
