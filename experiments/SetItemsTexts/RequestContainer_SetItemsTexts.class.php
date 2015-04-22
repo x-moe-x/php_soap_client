05790b31f3bd779b426da0a34bb4a8c7d0529b5f
xmoexdev <xmoexdev@gmail.com>
2015-04-22 12:09:29 +0200

    implemented setItemsTexts

--- experiments/SetItemsTexts/RequestContainer_SetItemsTexts.class.php
+++ experiments/SetItemsTexts/RequestContainer_SetItemsTexts.class.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once ROOT . 'includes/FillObjectFromArray.php';
 require_once ROOT . 'includes/RequestContainer.class.php';
 
 /**
@@ -7,14 +7,34 @@ require_once ROOT . 'includes/RequestContainer.class.php';
  */
 class RequestContainer_SetItemsTexts extends RequestContainer
 {
+	/**
+	 * @return RequestContainer_SetItemsTexts
+	 */
+	public function __construct()
+	{
+		parent::__construct(SoapCall_SetItemsTexts::MAX_ITEMS_PER_PAGES);
+	}
 
 	/**
 	 * returns the assembled request
 	 *
-	 * @return mixed
+	 * @return PlentySoapRequest_SetItemsTexts
 	 */
 	public function getRequest()
 	{
-		// TODO: Implement getRequest() method.
+		$request = new PlentySoapRequest_SetItemsTexts();
+
+		$request->ItemsList = new ArrayOfPlentysoapobject_setitemstexts();
+		$request->ItemsList->item = array();
+
+		foreach ($this->items as $item)
+		{
+			$texts = new PlentySoapObject_SetItemsTexts();
+			fillObjectFromArray($texts, $item);
+
+			$request->ItemsList->item[] = $texts;
+		}
+
+		return $request;
 	}
 }
