0d0a2d66f52571f62815aa00dd4ee2158aea85f3
xmoexdev <xmoexdev@gmail.com>
2015-04-22 13:17:17 +0200

    implemented setitemsfreetextfields call

--- experiments/SetItemsFreeTextFields/RequestContainer_SetItemsFreeTextFields.class.php
+++ experiments/SetItemsFreeTextFields/RequestContainer_SetItemsFreeTextFields.class.php
@@ -8,11 +8,29 @@ require_once ROOT . 'includes/RequestContainer.class.php';
 class RequestContainer_SetItemsFreeTextFields extends RequestContainer {
 
 	/**
+	 * @return RequestContainer_SetItemsFreeTextFields
+	 */
+	public function __construct() {
+		parent::__construct( SoapCall_SetItemsFreeTextFields::MAX_ITEMS_PER_PAGES );
+	}
+
+	/**
 	 * returns the assembled request
 	 *
-	 * @return mixed
+	 * @return PlentySoapRequest_SetItemsFreeTextFields
 	 */
 	public function getRequest() {
-		// TODO: Implement getRequest() method.
+		$request = new PlentySoapRequest_SetItemsFreeTextFields();
+
+		$request->ItemsFreeTextsList       = new ArrayOfPlentysoapobject_setitemsfreetextfields();
+		$request->ItemsFreeTextsList->item = array();
+
+		foreach ( $this->items as $item ) {
+			$freeTextFields = new PlentySoapObject_SetItemsFreeTextFields();
+			fillObjectFromArray( $freeTextFields, $item );
+
+			$request->ItemsFreeTextsList->item[] = $freeTextFields;
+		}
+		return $request;
 	}
 }
--- experiments/SetItemsFreeTextFields/SoapCall_SetItemsFreeTextFields.class.php
+++ experiments/SetItemsFreeTextFields/SoapCall_SetItemsFreeTextFields.class.php
@@ -12,7 +12,47 @@ class SoapCall_SetItemsFreeTextFields extends PlentySoapCall {
 	 */
 	const MAX_ITEMS_PER_PAGES = 100;
 
+	/**
+	 * @return SoapCall_SetItemsFreeTextFields
+	 */
+	public function __construct() {
+		parent::__construct( __CLASS__ );
+	}
+
+	/**
+	 * overrides PlentySoapCall's execute() method
+	 *
+	 * @return void
+	 */
 	public function execute() {
-		// TODO: Implement execute() method.
+		$this->debug( __FUNCTION__ . ' writing ItemsFreeTexts...' );
+		try {
+			// get all free text fields for items to be written
+			$oDBResult = DBQuery::getInstance()->select( 'SELECT * FROM `SetItemsFreeTextFields`' );
+
+			// for every 100 Items ...
+			for ( $page = 0, $maxPage = ceil( $oDBResult->getNumRows() / self::MAX_ITEMS_PER_PAGES ); $page < $maxPage; $page ++ ) {
+				// ... prepare a separate request ...
+				$requestContainer = new RequestContainer_SetItemsFreeTextFields();
+
+				// ... fill in data
+				while ( ! $requestContainer->isFull() && ( $currentTextData = $oDBResult->fetchAssoc() ) ) {
+					$requestContainer->add( $currentTextData );
+				}
+
+				// do soap call to plenty
+				$response = $this->getPlentySoap()->SetItemsFreeTextFields( $requestContainer->getRequest() );
+
+				// ... if successful ...
+				if ( $response->Success == true ) {
+					// ... be quiet ...
+				} else {
+					// ... otherwise log error and try next request
+					$this->getLogger()->debug( __FUNCTION__ . ' Request Error' );
+				}
+			}
+		} catch ( Exception $e ) {
+			$this->onExceptionAction( $e );
+		}
 	}
 }
