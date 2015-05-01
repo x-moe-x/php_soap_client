7ff69aacdbc88f1dd785f511f65d17ddeb3a362f
xmoexdev <xmoexdev@gmail.com>
2015-05-01 14:34:47 +0200

    added ExtractProperties skeleton

---
+++ experiments/ExtractProperties/ExtractProperties.class.php
@@ -0,0 +1,19 @@
+<?php
+require_once ROOT . 'includes/NX_Executable.abstract.php';
+
+class ExtractProperties extends NX_Executable
+{
+	public function __construct()
+	{
+		parent::__construct(__CLASS__);
+	}
+
+	public function execute()
+	{
+		// load all tech data records
+
+		// extract all key/value pairs
+
+		// store them unclustered in db
+	}
+}
