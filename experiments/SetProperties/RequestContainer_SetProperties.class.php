0cef82a5d4c229c7d3909e32312369c311e5c146
xmoex <xmoexdev@gmail.com>
2015-05-20 10:49:30 +0200

    implemented request

--- experiments/SetProperties/RequestContainer_SetProperties.class.php
+++ experiments/SetProperties/RequestContainer_SetProperties.class.php
@@ -17,6 +17,52 @@ class RequestContainer_SetProperties extends RequestContainer
 	 */
 	public function getRequest()
 	{
-		// TODO: Implement getRequest() method.
+		$request = new PlentySoapRequest_SetProperties();
+		$request->Properties = new ArrayOfPlentysoapobject_setproperty();
+		$request->Properties->item = array();
+
+		foreach ($this->items as &$propertyData)
+		{
+			$property = new PlentySoapObject_SetProperty();
+
+			fillObjectFromArray($property, $propertyData, array(
+				'PropertyChoice' => null,
+				'AmazonList'     => null,
+			));
+
+			if (isset($propertyData['PropertyChoice']) && is_array($propertyData['PropertyChoice']))
+			{
+				$property->PropertyChoice = new ArrayOfPlentysoapobject_setpropertychoice();
+				$property->PropertyChoice->item = array();
+
+				foreach ($propertyData['PropertyChoice'] as &$propertyChoiceData)
+				{
+					$propertyChoice = new PlentySoapObject_SetPropertyChoice();
+
+					fillObjectFromArray($propertyChoice, $propertyChoiceData);
+
+					$property->PropertyChoice->item[] = $propertyChoice;
+				}
+			}
+
+			if (isset($propertyData['AmazonList']) && is_array($propertyData['AmazonList']))
+			{
+				$property->AmazonList = new ArrayOfPlentysoapobject_setpropertyamazon();
+				$property->AmazonList->item = array();
+
+				foreach ($propertyData['AmazonList'] as &$amazonListData)
+				{
+					$amazonList = new PlentySoapObject_SetPropertyAmazon();
+
+					fillObjectFromArray($amazonList, $amazonListData);
+
+					$property->AmazonList->item[] = $amazonList;
+				}
+			}
+
+			$request->Properties->item[] = $property;
+		}
+
+		return $request;
 	}
 }
