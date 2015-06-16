41085bf78bae7e47ef54daf457715163b994705a
xmoex <xmoexdev@gmail.com>
2015-06-16 12:57:37 +0200

    implemented removepropertyfromitem call

--- experiments/RemovePropertyFromItem/SoapCall_RemovePropertyFromItem.class.php
+++ experiments/RemovePropertyFromItem/SoapCall_RemovePropertyFromItem.class.php
@@ -1,5 +1,6 @@
 <?php
 require_once ROOT . 'lib/soap/call/PlentySoapCall.abstract.php';
+require_once 'RequestContainer_RemovePropertyFromItem.class.php';
 
 /**
  * Class SoapCall_RemovePropertyFromItem
@@ -27,11 +28,57 @@ class SoapCall_RemovePropertyFromItem extends PlentySoapCall
 		$this->debug(__FUNCTION__ . ' remove properties to items...');
 		try
 		{
-			//TODO remove after debugging
-			$this->debug(__FUNCTION__ . ' NOT YET IMPLEMENTED');
+			// 1. get all property-item-pair records
+			$unwrittenPropertyFromItemData = DBQuery::getInstance()->select($this->getQuery());
+
+			$this->getLogger()->debug(__FUNCTION__ . ' found ' . $unwrittenPropertyFromItemData->getNumRows() . ' records...');
+
+			// 2. for every 100 updates ...
+			for ($page = 0, $maxPage = ceil($unwrittenPropertyFromItemData->getNumRows() / self::MAX_PROPERTIES_FROM_ITEMS_PER_PAGE); $page < $maxPage; $page++)
+			{
+				// ... prepare a separate request ...
+				$request = new RequestContainer_RemovePropertyFromItem(self::MAX_PROPERTIES_FROM_ITEMS_PER_PAGE);
+
+				// ... fill in data
+				$writtenUpdates = array();
+				while (!$request->isFull() && ($unwrittenRemoveData = $unwrittenPropertyFromItemData->fetchAssoc()))
+				{
+					$request->add(array(
+						'ItemId'     => $unwrittenRemoveData['ItemId'],
+						'PropertyId' => $unwrittenRemoveData['PropertyId'],
+					));
+
+					$writtenUpdates[] = $unwrittenRemoveData['id'];
+				}
+
+				// 3. write them back via soap
+				$response = $this->getPlentySoap()->RemovePropertyFromItem($request->getRequest());
+
+				// 4. if successful ...
+				if ($response->Success == true)
+				{
+					// ... then delete specified elements from setCurrentStocks
+					DBQuery::getInstance()->delete('DELETE FROM RemovePropertyFromItem WHERE id IN (' . implode(',', $writtenUpdates) . ')');
+				} else
+				{
+					// ... otherwise log error and try next request
+					$this->getLogger()->debug(__FUNCTION__ . ' Request Error');
+				}
+			}
+
 		} catch (Exception $e)
 		{
 			$this->onExceptionAction($e);
 		}
+	}
+
+	private function getQuery()
+	{
+		return 'SELECT
+	id,
+	ItemId,
+	PropertyId
+FROM
+	RemovePropertyFromItem';
 	}
 }
