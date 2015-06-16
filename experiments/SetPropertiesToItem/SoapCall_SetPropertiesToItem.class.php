86384183749476308deb58bad72d1f16d6e6b1ea
xmoex <xmoexdev@gmail.com>
2015-06-16 12:57:51 +0200

    delete written updates from db

--- experiments/SetPropertiesToItem/SoapCall_SetPropertiesToItem.class.php
+++ experiments/SetPropertiesToItem/SoapCall_SetPropertiesToItem.class.php
@@ -26,27 +26,29 @@ class SoapCall_SetPropertiesToItem extends PlentySoapCall
 		try
 		{
 			// 1. get all tuples (itemID, propertyID, propertyItemValue)
-			$unwrittenItemToPropertyRecords = DBQuery::getInstance()->select('SELECT ItemId, Lang, PropertyId, PropertyItemValue FROM SetPropertiesToItem');
+			$unwrittenItemToPropertyRecords = DBQuery::getInstance()->select('SELECT id, ItemId, Lang, PropertyId, PropertyItemValue FROM SetPropertiesToItem');
 
 			$this->getLogger()->debug(__FUNCTION__ . ' found ' . $unwrittenItemToPropertyRecords->getNumRows() . ' records...');
 
 			// 2. for every 100 updates ...
 			for ($page = 0, $maxPage = ceil($unwrittenItemToPropertyRecords->getNumRows() / self::MAX_ITEMS_TO_PROPERTIES_PER_PAGE); $page < $maxPage; $page++)
 			{
+				$this->getLogger()->debug(__FUNCTION__ . ' page ' . ($page + 1) . ' of ' . $maxPage);
 				// ... prepare a separate request ...
 				$request = new RequestContainer_SetPropertiesToItem(self::MAX_ITEMS_TO_PROPERTIES_PER_PAGE);
 
 				// ... fill in data
+				$writtenUpdates = array();
 				while (!$request->isFull() && ($unwrittenItemToPropertyRecord = $unwrittenItemToPropertyRecords->fetchAssoc()))
-
 				{
-
 					$request->add(array(
 						'ItemId'            => $unwrittenItemToPropertyRecord['ItemId'],
 						'Lang'              => $unwrittenItemToPropertyRecord['Lang'],
 						'PropertyId'        => $unwrittenItemToPropertyRecord['PropertyId'],
 						'PropertyItemValue' => $unwrittenItemToPropertyRecord['PropertyItemValue'],
 					));
+
+					$writtenUpdates[] = $unwrittenItemToPropertyRecord['id'];
 				}
 
 				// 3. write them back via soap
@@ -55,15 +57,14 @@ class SoapCall_SetPropertiesToItem extends PlentySoapCall
 				// 4. if successful ...
 				if ($response->Success == true)
 				{
-					// ... do nothing
+					// ... then delete specified elements from setCurrentStocks
+					DBQuery::getInstance()->delete('DELETE FROM SetPropertiesToItem WHERE id IN (' . implode(',', $writtenUpdates) . ')');
 				} else
 				{
-					die(print_r($response));
 					// ... otherwise log error and try next request
 					$this->getLogger()->debug(__FUNCTION__ . ' Request Error');
 				}
 			}
-
 		} catch (Exception $e)
 		{
 			$this->onExceptionAction($e);
