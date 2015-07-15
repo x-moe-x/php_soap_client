a05362282d9351c3d30a4420ac21c2a849d282ca
xmoex <xmoexdev@gmail.com>
2015-07-15 13:21:44 +0200

    implemented get items texts request container

--- experiments/GetItemsTexts/RequestContainer_GetItemsTexts.class.php
+++ experiments/GetItemsTexts/RequestContainer_GetItemsTexts.class.php
@@ -14,6 +14,23 @@ class RequestContainer_GetItemsTexts extends RequestContainer
 	public function getRequest()
 	{
 		$request = new PlentySoapRequest_GetItemsTexts();
+		$request->ItemsList = new ArrayOfPlentysoapobject_requestitems();
+		$request->ItemsList->item = array();
+
+		foreach ($this->items as $item)
+		{
+			$itemRequest = new PlentySoapObject_RequestItems();
+
+			fillObjectFromArray($itemRequest, array(
+				'ExternalItemNumer' => null,
+				'ItemId'            => $item,
+				'ItemNumber'        => null,
+				'Lang'              => 'de',
+			));
+
+			$request->ItemsList->item[] = $itemRequest;
+		}
+
 		return $request;
 	}
 }
