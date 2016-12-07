c61746f6f4db4cc4362191344e00df08648ed82c
xmoex <xmoexdev@gmail.com>
2016-12-07 13:51:52 +0100

    added pseudo call skeleton

---
+++ experiments/TransformProductFeatures/TransformProductFeatures.class.php
@@ -0,0 +1,17 @@
+<?php
+require_once ROOT . 'includes/NX_Executable.abstract.php';
+
+/**
+ * Class TransformProductFeatures
+ */
+class TransformProductFeatures extends NX_Executable
+{
+
+	/**
+	 *
+	 */
+	public function execute()
+	{
+
+	}
+}
