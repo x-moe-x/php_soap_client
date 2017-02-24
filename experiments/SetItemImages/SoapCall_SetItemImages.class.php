38b7de9698d91ba051fdf93bf74d7f6ca62daff8
xmoex <xmoexdev@gmail.com>
2017-02-24 11:10:22 +0100

    switch from optimized images upload to regular update

--- experiments/SetItemImages/SoapCall_SetItemImages.class.php
+++ experiments/SetItemImages/SoapCall_SetItemImages.class.php
@@ -11,7 +11,12 @@ class SoapCall_SetItemImages extends PlentySoapCall
 	/**
 	 * @var string
 	 */
-	const LOCAL_OPTIMIZED_IMAGE_PATH = '/kunden/homepages/22/d66025481/htdocs/plenty_item_images_optimised';
+	const LOCAL_IMAGE_PATH = '/kunden/homepages/22/d66025481/htdocs/upload_images';
+
+	/**
+	 * @var int
+	 */
+	const MAX_IMAGES_PER_REQUEST = 10;
 
 	/**
 	 * @var RequestContainer_SetItemImages[]
@@ -30,43 +35,72 @@ class SoapCall_SetItemImages extends PlentySoapCall
 	 */
 	public function execute()
 	{
-		// fetch all image url records
-		$dbResult = DBQuery::getInstance()->select('SELECT ItemID, ImageID, ImageURL FROM ItemsImages WHERE ItemID = 79');
+		// fetch all image update records
+		$dbResult = DBQuery::getInstance()
+			->select('SELECT * FROM SetItemImages');
 
-		$this->debug(__FUNCTION__ . ': found ' . $dbResult->getNumRows() . ' item image url records');
+		$this->debug(__FUNCTION__ . ': found ' . $dbResult->getNumRows() . ' item image records');
 
 		// process them indiviually
 		$i = 0;
 		while ($currentItem = $dbResult->fetchAssoc())
 		{
-			if ($i % 100 === 0)
+			if ($i % 50 === 0)
 			{
-				$this->getLogger()->info(__FUNCTION__ . ': processing record ' . $i);
+				$this->getLogger()
+					->info(__FUNCTION__ . ': processing record ' . $i);
 			}
 			$i++;
 
-			// match against relPath and fName pattern
-			if (preg_match("/^.+:\\/\\/.*?(?'relPath'\\/.*?\\/)(?'fName'[\\w\\d-+_]+\\.\\w+)$/", $currentItem['ImageURL'], $matches))
-			{
-				// check path if there's an optimised image...
-				$path = self::LOCAL_OPTIMIZED_IMAGE_PATH . '/' . $matches['relPath'];
-				if (file_exists($path . '/' . $matches['fName']))
-				{
-					// ... then prepare upload
-					if (!array_key_exists($currentItem['ItemID'], $this->preparedRequests))
-					{
-						$this->preparedRequests[$currentItem['ItemID']] = new RequestContainer_SetItemImages();
-					}
-					$this->preparedRequests[$currentItem['ItemID']]->add($currentItem);
-				}
-			} // or terminate execution on error
-			else
-			{
-				$this->getLogger()->crit(__FUNCTION__ . ': could not match' . $currentItem['ImageURL'] . ' against relPath & fName Pattern');
-				die();
+			$itemId = intval($currentItem['ItemID']);
+			if (!array_key_exists($itemId, $this->preparedRequests)) {
+				$this->preparedRequests[$itemId] = new RequestContainer_SetItemImages($itemId, self::MAX_IMAGES_PER_REQUEST);
 			}
+
+			// prepare an update
+			$data = array(
+				'Position'         => intval($currentItem['Position']),
+				'AttributeValueId' => $currentItem['AttributeValueId'],
+				'ImageID'          => $currentItem['ImageID'],
+				'ImageFileName'    => $currentItem['ImageFileName'],
+				'ImageFileEnding'  => $currentItem['ImageFileEnding'],
+				'ImageURL'         => $currentItem['ImageURL'],
+				'AlternativeText'  => $currentItem['AlternativeText'],
+				'Name'             => $currentItem['Name'],
+				'Lang'             => $currentItem['Lang'],
+				'DeleteName'       => $currentItem['DeleteName'],
+				'ImageFilePath'    => null,
+			);
+
+			if (!is_null($currentItem['ImageFileLocalPath'])) {
+				// ... then also prepare upload
+				$path = self::LOCAL_IMAGE_PATH . '/' . $currentItem['ImageFileLocalPath'];
+				if (file_exists($path)) {
+					$data['ImageFilePath'] = $path;
+				} else {
+					throw new RuntimeException("Missing File for ItemID $itemId: $path");
+				}
+			}
+
+			$this->preparedRequests[$itemId]->add($data);
 		}
 
-		// perform upload
+		// perform upload/update
+		try {
+			foreach ($this->preparedRequests as $request) {
+				$response = $this->getPlentySoap()
+					->SetItemImages($request->getRequest());
+				if ($response->Success) {
+					// do nothing
+				} else {
+					print_r($response);
+					die();
+				}
+
+			}
+		} catch
+		(Exception $e) {
+			$this->onExceptionAction($e);
+		}
 	}
 }
