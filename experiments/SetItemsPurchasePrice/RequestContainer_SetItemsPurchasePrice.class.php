f048f5f0eb3601dfac52bbcb18759f2ad8e7ca84
xmoex <xmoexdev@gmail.com>
2017-03-03 14:27:17 +0100

    implemented request container

--- experiments/SetItemsPurchasePrice/RequestContainer_SetItemsPurchasePrice.class.php
+++ experiments/SetItemsPurchasePrice/RequestContainer_SetItemsPurchasePrice.class.php
@@ -3,13 +3,42 @@
 require_once ROOT . 'includes/FillObjectFromArray.php';
 require_once ROOT . 'includes/RequestContainer.class.php';
 
+/**
+ * Class RequestContainer_SetItemsPurchasePrice
+ */
 class RequestContainer_SetItemsPurchasePrice extends RequestContainer {
 
 	/**
 	 * returns the assembled request
-	 * @return mixed
+	 * @return PlentySoapRequest_SetItemsPurchasePrice
 	 */
 	public function getRequest() {
-		// TODO: Implement getRequest() method.
+		$result = new PlentySoapRequest_SetItemsPurchasePrice();
+		$result->ItemsPurchasePrice = new ArrayOfPlentysoapobject_setitemspurchaseprice();
+		$result->ItemsPurchasePrice->item = array();
+
+		foreach ($this->items as $current) {
+			$purchasePriceUpdate = new PlentySoapObject_SetItemsPurchasePrice();
+
+			if (array_key_exists('SKU', $current)) {
+				$sku = $current['SKU'];
+			} elseif (array_key_exists('ItemID', $current)) {
+				$priceId = 0;
+				$avsId = 0;
+				if (array_key_exists('PriceID', $current)) {
+					$priceId = $current['PriceID'];
+				}
+				if (array_key_exists('AttributeValueSetID', $current)) {
+					$avsId = $current['AttributeValueSetID'];
+				}
+				$sku = Values2SKU($current['ItemID'], $avsId, $priceId);
+			}
+			$purchasePriceUpdate->SKU = $sku;
+			$purchasePriceUpdate->PurchasePrice = $current['PurchasePrice'];
+
+			$result->ItemsPurchasePrice->item[] = $purchasePriceUpdate
+		}
+
+		return $result;
 	}
 }
