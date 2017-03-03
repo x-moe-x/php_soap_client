679515fca76740753953201ad26faa147620cab1
xmoex <xmoexdev@gmail.com>
2017-03-03 14:16:00 +0100

    added call skeleton

---
+++ experiments/SetItemsPurchasePrice/RequestContainer_SetItemsPurchasePrice.class.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once ROOT . 'includes/FillObjectFromArray.php';
+require_once ROOT . 'includes/RequestContainer.class.php';
+
+class RequestContainer_SetItemsPurchasePrice extends RequestContainer {
+
+	/**
+	 * returns the assembled request
+	 * @return mixed
+	 */
+	public function getRequest() {
+		// TODO: Implement getRequest() method.
+	}
+}
---
+++ experiments/SetItemsPurchasePrice/SoapCall_SetItemsPurchasePrice.class.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once ROOT . 'lib/soap/call/PlentySoapCall.abstract.php';
+require_once 'RequestContainer_SetItemsPurchasePrice.class.php';
+
+class SoapCall_SetItemsPurchasePrice extends PlentySoapCall {
+
+	public function execute() {
+		// TODO: Implement execute() method.
+	}
+}
